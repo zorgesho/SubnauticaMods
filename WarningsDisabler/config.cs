461df813f2058acfb2309270e5d92f84d58fce58
zorgesho <53096970+zorgesho@users.noreply.github.com>
2019-10-01 22:11:19 +0300

    WarningsDisabler initial commit

---
+++ WarningsDisabler/config.cs
@@ -0,0 +1,10 @@
+﻿using Common.Config;
+
+namespace WarningsDisabler
+{
+	[System.Serializable]
+	class Config
+	{
+		public int field = 42;
+	}
+}
---
+++ WarningsDisabler/main.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+using Harmony;
+
+namespace WarningsDisabler
+{
+	//using static Common.Config.ConfigHelper;
+
+	static public class Main
+	{
+		//static public Config config = tryLoadConfig<Config>();
+
+		static public void patch()
+		{
+			HarmonyInstance.Create("WarningsDisabler").PatchAll(Assembly.GetExecutingAssembly());
+		}
+	}
+}

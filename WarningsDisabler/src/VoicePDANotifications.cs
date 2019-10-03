7442b16872d45e81415db21a3dc7f2d5cce5fcbe
zorgesho <53096970+zorgesho@users.noreply.github.com>
2019-10-03 20:00:27 +0300

    Other notifications can be blocked now

--- WarningsDisabler/src/VoicePDANotifications.cs
+++ WarningsDisabler/src/VoicePDANotifications.cs
@@ -1,54 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
+
 using Harmony;
-using UnityEngine;
+
 using Common;
 
 namespace WarningsDisabler
 {
-	//"CyclopsWelcomeAboard": "CYCLOPS: Welcome aboard captain. All systems online.",
-	//"CyclopsWelcomeAboardAttention": "CYCLOPS: Welcome aboard captain. Some systems require attention.",
-	
-	//"SeamothWelcomeAboard" : "Seamoth: Welcome aboard captain.",
-	//"SeamothWelcomeNoPower" : "Seamoth: Warning: Emergency power only. Oxygen production offline.",
-	
-	//"ExosuitWelcomeAboard" : "PRAWN: Welcome aboard captain.",
-	//"ExosuitWelcomeNoPower" : "PRAWN: Warning: Emergency power only. Oxygen production offline.",
-	
-	//"BaseWelcomeAboard": "HABITAT: Welcome aboard captain.",
-	//"BaseWelcomeNoPower": "HABITAT: Warning: Emergency power only. Oxygen production offline.",
-	
-	//"BasePowerUp": "HABITAT: Power restored. All primary systems online.",
-	//"BasePowerDown": "HABITAT: Warning, emergency power only.",
-
 	[HarmonyPatch(typeof(VoiceNotification), "Play", new Type[] { typeof(object[]) })]
-	class VoiceNotification_Play_Patch
+	static class VoiceNotification_Play_Patch
 	{
-		static bool Prefix(VoiceNotification __instance, object[] args, bool __result)
+		static List<string> powerMessages = new List<string>()
 		{
-			$"VoiceNotification {__instance.text}".onScreen().logDbg();
+			"BasePowerUp",		// "HABITAT: Power restored. All primary systems online."
+			"BasePowerDown"		// "HABITAT: Warning, emergency power only."
+		};
+
+		static List<string> welcomeMessages = new List<string>()
+		{
+			"CyclopsWelcomeAboard",				// "CYCLOPS: Welcome aboard captain. All systems online."
+			"CyclopsWelcomeAboardAttention",	// "CYCLOPS: Welcome aboard captain. Some systems require attention."
+			"SeamothWelcomeAboard",				// "Seamoth: Welcome aboard captain."
+			"SeamothWelcomeNoPower",			// "Seamoth: Warning: Emergency power only. Oxygen production offline."
+			"ExosuitWelcomeAboard",				// "PRAWN: Welcome aboard captain."
+			"ExosuitWelcomeNoPower",			// "PRAWN: Warning: Emergency power only. Oxygen production offline."
+			"BaseWelcomeAboard",				// "HABITAT: Welcome aboard captain."
+			"BaseWelcomeNoPower"				// "HABITAT: Warning: Emergency power only. Oxygen production offline."
+		};
+		
+		static bool Prefix(VoiceNotification __instance, object[] args, bool __result)
+		{																											$"VoiceNotification.Play {__instance.text}, interval:{__instance.minInterval}".onScreen().logDbg();
+			if (Main.config.disablePowerWarnings && powerMessages.Find((s) => __instance.text == s) != null)
+				return false;
+			
+			if (Main.config.disableWelcomesMessages && welcomeMessages.Find((s) => __instance.text == s) != null)
+				return false;
+
 			return true;
 		}
 	}
-	
-	
 
-	//"DepthWarning100": "Warning: Passing 100 meters. Oxygen efficiency decreased.",
-	//"DepthWarning200": "Warning: Passing 200 meters. Oxygen efficiency greatly decreased.",
-	
-	//"VitalsOk": "Vital signs stabilizing.",
-	//"FoodLow": "Calorie intake recommended.",
-	//"FoodCritical": "Emergency, starvation imminent. Seek calorie intake immediately.",
-	//"FoodVeryLow": "Seek calorie intake.",
-	//"WaterLow": "Seek fluid intake.",
-	//"WaterCritical": "Seek fluid intake immediately.",
-	//"WaterVeryLow": "Seek fluid intake.",
 	
 	[HarmonyPatch(typeof(PDANotification), "Play", new Type[] { typeof(object[]) })]
-	class PDANotification_Play_Patch
+	static class PDANotification_Play_Patch
 	{
-		static bool Prefix(PDANotification __instance, object[] args)
+		static List<string> depthWarnings = new List<string>()
 		{
-			$"PDANotification {__instance.text}".onScreen().logDbg();
+			"DepthWarning100",	// "Warning: Passing 100 meters. Oxygen efficiency decreased."
+			"DepthWarning200"	// "Warning: Passing 200 meters. Oxygen efficiency greatly decreased."
+		};
+
+		static List<string> foodWaterWarnings = new List<string>()
+		{
+			"VitalsOk",			// "Vital signs stabilizing."
+			"FoodLow",			// "Calorie intake recommended."
+			"FoodCritical",		// "Emergency, starvation imminent. Seek calorie intake immediately."
+			"FoodVeryLow",		// "Seek calorie intake."
+			"WaterLow",			// "Seek fluid intake."
+			"WaterCritical",	// "Seek fluid intake immediately."
+			"WaterVeryLow"		// "Seek fluid intake."
+		};
+
+		static bool Prefix(PDANotification __instance, object[] args)
+		{																											$"PDANotification.Play {__instance.text}".onScreen().logDbg();
+			if (Main.config.disableDepthWarnings && depthWarnings.Find((s) => __instance.text == s) != null)
+				return false;
+			
+			if (Main.config.disableFoodWaterWarnings && foodWaterWarnings.Find((s) => __instance.text == s) != null)
+				return false;
+
 			return true;
 		}
 	}

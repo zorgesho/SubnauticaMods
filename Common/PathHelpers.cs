9fbbdf6974de39b14263773dc460eebc2f935653
zorgesho <53096970+zorgesho@users.noreply.github.com>
2019-10-03 23:40:52 +0300

    Removed unnecessary stuff from PathHelpers.cs

--- Common/PathHelpers.cs
+++ Common/PathHelpers.cs
@@ -3,15 +3,8 @@ using System.Reflection;
 
 namespace Common.PathHelpers
 {
-	class ModPath
+	static class ModPath
 	{
 		static public readonly string rootPath = Path.GetDirectoryName(Assembly.GetCallingAssembly().Location) + Path.DirectorySeparatorChar.ToString();
-
-		string path;
-
-		public ModPath(string _path) => path = rootPath + _path;
-
-		static public implicit operator string(ModPath p) => p.path;
-		static public explicit operator ModPath(string s) => new ModPath(s);
 	}
 }

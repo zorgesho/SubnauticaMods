60e0df2e4d2dc42fdf69cd3eba940622cccaa855
zorgesho <53096970+zorgesho@users.noreply.github.com>
2019-10-07 03:39:30 +0300

    UnityExtensions.cs added

---
+++ Common/UnityExtensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Common
+{
+	static class ObjectAndComponentExtensions
+	{
+		static public T getOrAddComponent<T>(this GameObject go) where T: Component
+		{
+			return go.GetComponent<T>() ?? go.AddComponent<T>();
+		}
+
+		static public void addComponentIfNeeded<T>(this GameObject go) where T: Component
+		{
+			if (!go.GetComponent<T>())
+				go.AddComponent<T>();
+		}
+	}
+}

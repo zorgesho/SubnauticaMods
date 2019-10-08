9fb2cdd739d73e0362edc9bbceacdda77c44fd60
zorgesho <53096970+zorgesho@users.noreply.github.com>
2019-10-08 18:19:33 +0300

    Common.GameSerialization project added

---
+++ Common/Common.GameSerialization/SaveLoad.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+using Oculus.Newtonsoft.Json;
+
+namespace Common.GameSerialization
+{
+	static class SaveLoad
+	{
+		static public void save<T>(string id, T saveData)
+		{
+			string filePath = Path.Combine(PathHelpers.ModPath.savesPath, id + ".json");
+
+			File.WriteAllText(filePath, JsonConvert.SerializeObject(saveData, Formatting.None));
+		}
+
+		static public T load<T>(string id)
+		{
+			string filePath = Path.Combine(PathHelpers.ModPath.savesPath, id + ".json");
+
+			return File.Exists(filePath)? JsonConvert.DeserializeObject<T>(File.ReadAllText(filePath)): default;
+		}
+	}
+}

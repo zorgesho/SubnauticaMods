060430e78df386ecf72c3f801e39518ba6d535d9
zorgesho <53096970+zorgesho@users.noreply.github.com>
2019-10-08 18:20:33 +0300

    Common.GameSerialization project added

--- Common/PathHelpers.cs
+++ Common/PathHelpers.cs
@@ -6,5 +6,22 @@ namespace Common.PathHelpers
 	static class ModPath
 	{
 		static public readonly string rootPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + Path.DirectorySeparatorChar.ToString();
+
+		static public string savesPath
+		{
+			get
+			{
+#if DEBUG && !GAME_SAVEPATH
+				string path = Path.Combine(rootPath, "{dbgsave}");
+
+				if (!Directory.Exists(path))
+					Directory.CreateDirectory(path);														$"Using DEBUG SAVE PATH '{path}'".logWarning();
+
+				return path;
+#else
+				return Path.Combine(SaveLoadManager.GetTemporarySavePath(), Strings.modName);
+#endif
+			}
+		}
 	}
 }

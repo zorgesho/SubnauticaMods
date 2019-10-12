e9cf489d3910256b9587127fd58731d60f731ad1
zorgesho <53096970+zorgesho@users.noreply.github.com>
2019-10-12 05:10:52 +0300

    Misc.cs added

---
+++ Common/Misc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Code Quality", "IDE0051", Scope = "namespaceanddescendants", Target = "Common")]
+[assembly: SuppressMessage("Code Quality", "IDE0060", Scope = "namespaceanddescendants", Target = "Common")]
+
+namespace Common
+{
+	static class Debug
+	{
+		// based on code from http://www.csharp-examples.net/reflection-callstack/
+		static public void logStack(string msg = "")
+		{
+			StackTrace stackTrace = new StackTrace();
+			StackFrame[] stackFrames = stackTrace.GetFrames();
+			
+			string output = $"Callstack {msg}:" + Environment.NewLine;
+
+			for (int i = 1; i < stackFrames.Length; ++i) // dont print first item, it is "printStack"
+			{
+				MethodBase method = stackFrames[i].GetMethod();
+				output += $"\t{method.DeclaringType.Name}.{method.Name}" + Environment.NewLine;
+			}
+
+			output.log();
+		}
+	}
+}

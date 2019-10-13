cc3683302b5ab60fd8eb53fd9ebeb98c0a329b33
zorgesho <53096970+zorgesho@users.noreply.github.com>
2019-10-14 01:30:40 +0300

    Split Patches.cs

---
+++ ConsoleImproved/src/ConsoleHelper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using Common;
+
+namespace ConsoleImproved
+{
+	static partial class ConsoleHelper
+	{
+		static GameObject consoleObject = null;
+		static readonly string historyPath = Common.PathHelper.Paths.modRootPath + "history.txt";
+
+		class ConsoleCommands: PersistentConsoleCommands
+		{
+			void OnConsoleCommand_clearhistory(NotificationCenter.Notification n)
+			{
+				setHistory(new List<string>());
+			}
+		}
+
+
+		static void init()
+		{
+			if (consoleObject == null)
+			{
+				consoleObject = PersistentConsoleCommands.createGameObject<ConsoleCommands>();
+				DevConsole.disableConsole = !Main.config.consoleEnabled;
+			}
+		}
+
+
+		static void saveHistory()
+		{
+			if (File.Exists(historyPath)) // TODO: check permissions, maybe trycatch
+				File.Delete(historyPath);
+			
+			List<string> history = DevConsole.instance.history;
+
+			// save 'historySizeToSave' last history entries or all history if historySizeToSave == 0
+			int i = Main.config.historySizeToSave == 0? 0: Mathf.Max(0, history.Count - Main.config.historySizeToSave);
+
+			StringBuilder stringBuilder = new StringBuilder();
+			while (i < history.Count)
+				stringBuilder.AppendLine(history[i++]);
+
+			File.WriteAllText(historyPath, stringBuilder.ToString());
+		}
+		
+		
+		static void loadHistory()
+		{
+			if (!File.Exists(historyPath))
+				return;
+
+			string loadedHistory = File.ReadAllText(historyPath);
+
+			if (!string.IsNullOrEmpty(loadedHistory))
+			{
+				string[] lines = loadedHistory.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+				setHistory(new List<string>(lines));
+			}
+		}
+
+
+		static void setHistory(List<string> history)
+		{
+			DevConsole.instance.history = history;
+			DevConsole.instance.inputField.SetHistory(history);
+		}
+	}
+}
--- ConsoleImproved/src/Patches.cs
+++ ConsoleImproved/src/Patches.cs
@@ -1,62 +1,62 @@
-﻿using System;
-using System.IO;
-using System.Text;
-using System.Reflection.Emit;
+﻿using System.Reflection.Emit;
 using System.Collections.Generic;
 
-using UnityEngine;
 using Harmony;
-
-using Common;
 
 namespace ConsoleImproved
 {
-	// patch for full history in console
-	[HarmonyPatch(typeof(ConsoleInput), "Validate")]
-	static class ConsoleInput_Validate_Patch
+	static partial class ConsoleHelper
 	{
-		static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		// patch for full history in console
+		[HarmonyPatch(typeof(ConsoleInput), "Validate")]
+		static class ConsoleInput_Validate_Patch
 		{
-			bool injected = false;
-
-			foreach (var instruction in instructions)
+			static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
 			{
-				if (!injected && instruction.opcode.Equals(OpCodes.Ldfld))
-				{
-					yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(ConsoleInput), "historyIndex"));
-					continue;
-				}
+				bool injected = false;
 
-				if (!injected && instruction.opcode.Equals(OpCodes.Callvirt))
+				foreach (var instruction in instructions)
 				{
-					injected = true;
-					yield return new CodeInstruction(OpCodes.Nop);
-					continue;
-				}
+					if (!injected && instruction.opcode.Equals(OpCodes.Ldfld))
+					{
+						yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(ConsoleInput), "historyIndex"));
+						continue;
+					}
 
-				yield return instruction;
+					if (!injected && instruction.opcode.Equals(OpCodes.Callvirt))
+					{
+						injected = true;
+						yield return new CodeInstruction(OpCodes.Nop);
+						continue;
+					}
+
+					yield return instruction;
+				}
+			}
+		}
+
+
+		[HarmonyPatch(typeof(DevConsole), "Awake")]
+		static class DevConsole_Awake_Patch
+		{
+			static void Postfix(DevConsole __instance)
+			{
+				init();
+				loadHistory();
+			}
+		}
+
+
+		[HarmonyPatch(typeof(DevConsole), "OnDisable")]
+		static class DevConsole_OnDisable_Patch
+		{
+			static void Postfix(DevConsole __instance)
+			{
+				saveHistory();
 			}
 		}
 	}
 
-	[HarmonyPatch(typeof(DevConsole), "Awake")]
-	static class DevConsole_Awake_Patch
-	{
-		static void Postfix(DevConsole __instance)
-		{
-			ConsoleHelper.init();
-			ConsoleHelper.loadHistory();
-		}
-	}
-
-	[HarmonyPatch(typeof(DevConsole), "OnDisable")]
-	static class DevConsole_OnDisable_Patch
-	{
-		static void Postfix(DevConsole __instance)
-		{
-			ConsoleHelper.saveHistory();
-		}
-	}
 
 	//private bool KeyPressedOverride(Event evt)
 	//[HarmonyPatch(typeof(ConsoleInput), "KeyPressedOverride")]
@@ -75,68 +75,4 @@ namespace ConsoleImproved
 	//		return true;
 	//	}
 	//}
-
-
-	static class ConsoleHelper
-	{
-		static GameObject consoleObject = null;
-		static readonly string historyPath = Common.PathHelper.Paths.modRootPath + "history.txt";
-
-		static public void init()
-		{
-			if (consoleObject == null)
-			{
-				consoleObject = PersistentConsoleCommands.createGameObject<ConsoleCommands>();
-				DevConsole.disableConsole = !Main.config.consoleEnabled;
-			}
-		}
-
-		static public void saveHistory()
-		{
-			if (File.Exists(historyPath)) // TODO: check permissions, maybe trycatch
-				File.Delete(historyPath);
-			
-			List<string> history = DevConsole.instance.history;
-
-			// save 'historySizeToSave' last history entries or all history if historySizeToSave == 0
-			int i = Main.config.historySizeToSave == 0? 0: Mathf.Max(0, history.Count - Main.config.historySizeToSave);
-
-			StringBuilder stringBuilder = new StringBuilder();
-			while (i < history.Count)
-				stringBuilder.AppendLine(history[i++]);
-
-			File.WriteAllText(historyPath, stringBuilder.ToString());
-		}
-		
-		
-		static public void loadHistory()
-		{
-			if (!File.Exists(historyPath))
-				return;
-
-			string loadedHistory = File.ReadAllText(historyPath);
-
-			if (!string.IsNullOrEmpty(loadedHistory))
-			{
-				string[] lines = loadedHistory.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
-
-				setHistory(new List<string>(lines));
-			}
-		}
-
-		static public void setHistory(List<string> history)
-		{
-			DevConsole.instance.history = history;
-			DevConsole.instance.inputField.SetHistory(history);
-		}
-
-		
-		class ConsoleCommands: PersistentConsoleCommands
-		{
-			void OnConsoleCommand_clearhistory(NotificationCenter.Notification n)
-			{
-				setHistory(new List<string>());
-			}
-		}
-	}
 }

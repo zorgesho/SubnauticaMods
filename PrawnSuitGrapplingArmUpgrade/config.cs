a75f2b1b6192c6b46edd7252587e52b88cdf64e2
zorgesho <53096970+zorgesho@users.noreply.github.com>
2019-10-29 00:28:09 +0300

    PrawnSuitGrapplingArmUpgrade initial commit

---
+++ PrawnSuitGrapplingArmUpgrade/config.cs
@@ -0,0 +1,9 @@
+﻿using Common.Configuration;
+
+namespace PrawnSuitGrapplingArmUpgrade
+{
+	class ModConfig: Config
+	{
+		public readonly int field = 42;
+	}
+}
---
+++ PrawnSuitGrapplingArmUpgrade/main.cs
@@ -0,0 +1,15 @@
+﻿using Common;
+using Common.Configuration;
+
+namespace PrawnSuitGrapplingArmUpgrade
+{
+	static public class Main
+	{
+		static internal ModConfig config = Config.tryLoad<ModConfig>();
+
+		static public void patch()
+		{
+			HarmonyHelper.patchAll();
+		}
+	}
+}

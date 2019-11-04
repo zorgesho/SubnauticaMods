4e6cca1fbdd81e6bdea6fc8c979ffc8b3db69a61
zorgesho <53096970+zorgesho@users.noreply.github.com>
2019-11-04 13:14:18 +0300

    PrawnSuitSettings initial commit

---
+++ PrawnSuitSettings/config.cs
@@ -0,0 +1,9 @@
+﻿using Common.Configuration;
+
+namespace PrawnSuitSettings
+{
+	class ModConfig: Config
+	{
+		public readonly int field = 42;
+	}
+}
---
+++ PrawnSuitSettings/properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PrawnSuitSettings")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PrawnSuitSettings")]
+[assembly: AssemblyCopyright("© 2019 zorgesho")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("8ecfbd17-d9c6-4fa2-92e2-fb76f71aa316")]
+
+[assembly: AssemblyVersion("1.0.0.0")]

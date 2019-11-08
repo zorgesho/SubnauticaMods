ac0d79dcd266764ed431a675678186aa5efcd8a9
zorgesho <53096970+zorgesho@users.noreply.github.com>
2019-11-08 22:56:50 +0300

    AssetsHelper added

---
+++ Common/Common.AssetsHelper/AssetsHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace Common
+{
+	static class AssetsHelper
+	{
+		public static Atlas.Sprite loadSprite(string textureName)
+		{
+			return textureToSprite(loadTextureFromFile(Paths.assetsPath + textureName + ".png"));
+		}
+
+		static Texture2D loadTextureFromFile(string textureFilePath)
+		{
+			if (!File.Exists(textureFilePath))
+				return null;
+
+			Texture2D tex = new Texture2D(2, 2);
+			tex.LoadImage(File.ReadAllBytes(textureFilePath));
+
+			return tex;
+		}
+
+		static Atlas.Sprite textureToSprite(Texture2D tex)
+		{
+			return new Atlas.Sprite(Sprite.Create(tex, new Rect(0f, 0f, tex.width, tex.height), new Vector2(0.5f, 0.5f)));
+		}
+	}
+}
---
+++ Common/Common.AssetsHelper/PathHelper.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace Common
+{
+	static partial class Paths
+	{
+		public static readonly string assetsPath = modRootPath + "assets" + Path.DirectorySeparatorChar;
+	}
+}

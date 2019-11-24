e3185ec9650672c5c83c05327820355a94387ef1
zorgesho <53096970+zorgesho@users.noreply.github.com>
2019-11-24 15:07:14 +0300

    HabitatPlatform initial commit

---
+++ HabitatPlatform/config.cs
@@ -0,0 +1,9 @@
+﻿using Common.Configuration;
+
+namespace HabitatPlatform
+{
+	class ModConfig: Config
+	{
+		public readonly int field = 42;
+	}
+}
---
+++ HabitatPlatform/properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HabitatPlatform")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HabitatPlatform")]
+[assembly: AssemblyCopyright("© 2019 zorgesho")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("c504563a-b554-4a91-ae30-14cfcd626007")]
+
+[assembly: AssemblyVersion("1.0.0.0")]

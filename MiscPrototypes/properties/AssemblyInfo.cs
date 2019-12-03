c9e26fe346db3b1fef7ababa13d04d7f6f8a62c9
zorgesho <53096970+zorgesho@users.noreply.github.com>
2019-12-03 12:18:00 +0300

    MiscPrototypes added

---
+++ MiscPrototypes/properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MiscPrototypes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MiscPrototypes")]
+[assembly: AssemblyCopyright("© 2019 zorgesho")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("5322339c-9933-4fb2-a04a-495718a3f66b")]
+
+[assembly: AssemblyVersion("1.0.0.0")]

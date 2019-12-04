ae5e79b34d9325c2abfffcc7de9eb6602e72db68
zorgesho <53096970+zorgesho@users.noreply.github.com>
2019-12-04 13:43:39 +0300

    dumpobjects commands added

--- ConsoleImproved/src/ConsoleCommands.cs
+++ ConsoleImproved/src/ConsoleCommands.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 
+using UnityEngine;
+
 using Common;
 
 namespace ConsoleImproved
@@ -19,7 +21,7 @@ namespace ConsoleImproved
 			void OnConsoleCommand_clear(NotificationCenter.Notification _)
 			{
 				foreach (var m in ErrorMessage.main.messages)
-					m.timeEnd = UnityEngine.Time.time - 1;
+					m.timeEnd = Time.time - 1;
 			}
 
 
@@ -35,7 +37,7 @@ namespace ConsoleImproved
 			}
 
 
-			void OnConsoleCommand_prefabdump(NotificationCenter.Notification n)
+			void OnConsoleCommand_dumpprefab(NotificationCenter.Notification n)
 			{
 				if (n.getArgsCount() == 0)
 					return;
@@ -61,6 +63,47 @@ namespace ConsoleImproved
 
 				"Dump complete".onScreen();
 			}
+
+
+			void OnConsoleCommand_dumpobjects(NotificationCenter.Notification n)
+			{
+				Type getComponentType(string typeName)
+				{
+					foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+					{
+						Type type = assembly.GetType(typeName, false, true);
+
+						if (type != null && typeof(Component).IsAssignableFrom(type))
+							return type;
+					}
+
+					return null;
+				}
+
+				if (n.getArgsCount() == 0)
+					return;
+
+				string cmtTypeName = n.getArg(0) as string;
+				Type cmpType = getComponentType(cmtTypeName);
+
+				if (cmpType != null && FindObjectsOfType(cmpType) is Component[] cmps)
+					StartCoroutine(nameof(_dumpObjects), cmps);
+			}
+
+			IEnumerator _dumpObjects(Component[] cmps)
+			{
+				$"Objects to dump: {cmps.Length}".onScreen();
+
+				int index = 0;
+				foreach (var cmp in cmps)
+				{
+					cmp.gameObject.dump(cmp.gameObject.name + "_" + index++);
+					yield return null;
+				}
+
+				"Dump complete".onScreen();
+			}
+
 
 			void OnConsoleCommand_printcfgvars(NotificationCenter.Notification n)
 			{

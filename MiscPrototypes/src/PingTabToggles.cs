306029daf3b640a2d81e229feeb81b4478570386
zorgesho <53096970+zorgesho@users.noreply.github.com>
2019-12-10 21:24:14 +0300

    some prototypes for ui improvements

---
+++ MiscPrototypes/src/PingTabToggles.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Harmony;
+
+
+using Common;
+
+namespace MiscPrototypes
+{
+	static class TestPingToggles
+	{
+		public static void init()
+		{
+			GameObject pingTab = Player.main.GetPDA().ui.gameObject.getChild("Content/PingManagerTab/Content");
+			GameObject buttonAll = pingTab.getChild("ButtonAll");
+
+			GameObject button1 = Object.Instantiate(buttonAll);
+			button1.setParent(pingTab, false);
+
+			//button1.GetComponent<RectTransform>().copyValuesFrom(buttonAll.GetComponent<RectTransform>());
+			button1.GetComponent<RectTransform>().anchoredPosition =  buttonAll.GetComponent<RectTransform>().anchoredPosition;
+			button1.GetComponent<RectTransform>().anchoredPosition3D =  buttonAll.GetComponent<RectTransform>().anchoredPosition3D;
+			button1.GetComponent<RectTransform>().localPosition =  buttonAll.GetComponent<RectTransform>().localPosition;
+			button1.GetComponent<RectTransform>().localScale =  buttonAll.GetComponent<RectTransform>().localScale;
+			button1.GetComponent<RectTransform>().offsetMin =  buttonAll.GetComponent<RectTransform>().offsetMin;
+			button1.GetComponent<RectTransform>().offsetMax =  buttonAll.GetComponent<RectTransform>().offsetMax;
+
+			button1.GetComponent<Toggle>().onValueChanged.RemoveAllListeners();
+			button1.GetComponent<Toggle>().onValueChanged = null;
+			//button1.GetComponent<Toggle>().onValueChanged.AddListener(delegate { ToggleValueChanged(null); });
+
+			Vector3 pos = button1.transform.localPosition;
+			pos.x += 50;
+			button1.transform.localPosition = pos;
+			//GameObject button1 = buttonAll;
+			////button1.setParent(pingTab, false);
+			//Vector3 pos = button1.transform.localPosition;
+			//pos.x += 50;
+			//button1.transform.localPosition = pos;
+		}
+
+		static void ToggleValueChanged(Toggle change)
+		{
+			"LLL".onScreen();
+		}
+	}
+	
+	[HarmonyPatch(typeof(uGUI_PingTab), "SetEntriesVisibility")]
+	class Player_Update_Patchsdf
+	{
+		static void Postfix()
+		{
+			Common.Debug.logStack();
+		}
+	}
+
+	[HarmonyPatch(typeof(Player), "Update")]
+	class Player_Update_Patch
+	{
+		static void Postfix()
+		{
+			if (Input.GetKeyDown(KeyCode.PageUp))
+			{
+				TestPingToggles.init();
+			}
+		}
+	}
+}
---
+++ MiscPrototypes/src/SelectTabsWithKeys.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using Harmony;
+
+using Common;
+
+namespace MiscPrototypes
+{
+	using Debug = Common.Debug;
+
+	//[HarmonyPatch(typeof(Inventory), "ExecuteItemAction")]
+	//static class TooltipFactory_Recipe_Patchsdfsdf
+	//{
+	//	static void Prefix(ItemAction action)
+	//	{
+	//		$"{action}".log();
+	//		Debug.logStack();
+
+	//	}
+	//}
+	
+	//[HarmonyPatch(typeof(QuickSlots), "Assign")]
+	//static class TooltipFactory_Recipe_Patchssdfsdfdfsdf
+	//{
+	//	static void Prefix()
+	//	{
+	//	//	$"{action}".log();
+	//		Debug.logStack();
+
+	//	}
+	//}
+	
+	//[HarmonyPatch(typeof(QuickSlots), "Bind")]
+	//static class TooltipFactory_Recipe_Patcsdfhssdfsdfdfsdf
+	//{
+	//	static void Prefix()
+	//	{
+	//	//	$"{action}".log();
+	//		Debug.logStack();
+
+	//	}
+	//}
+	
+	[HarmonyPatch(typeof(Player), "Update")]
+	static class TooltipFactorysdf_Recipe_Patcsdfhssdfsdfdfsdf
+	{
+		static void Postfix()
+		{
+			TabKeys.update();
+		}
+	}
+
+
+	static class TabKeys
+	{
+		static bool checkPDA()
+		{
+			Player main = Player.main;
+			if (main.GetPDA().isInUse)
+			{
+				InventoryItem hoveredItem = ItemDragManager.hoveredItem;
+				if (hoveredItem != null && Inventory.main.GetCanBindItem(hoveredItem))
+					return true;
+			}
+
+			return false;
+		}
+		
+		public static void update()
+		{
+			if (uGUI_BuilderMenu.singleton.state)
+			{
+				if (Input.GetKeyDown(KeyCode.Alpha1))
+					uGUI_BuilderMenu.singleton.SetCurrentTab(0);
+				if (Input.GetKeyDown(KeyCode.Alpha2))
+					uGUI_BuilderMenu.singleton.SetCurrentTab(1);
+				if (Input.GetKeyDown(KeyCode.Alpha3))
+					uGUI_BuilderMenu.singleton.SetCurrentTab(2);
+				if (Input.GetKeyDown(KeyCode.Alpha4))
+					uGUI_BuilderMenu.singleton.SetCurrentTab(3);
+				if (Input.GetKeyDown(KeyCode.Alpha5))
+					uGUI_BuilderMenu.singleton.SetCurrentTab(4);
+			}
+			
+			
+			if (Input.GetKeyDown(KeyCode.Alpha1))
+			{
+				if (!checkPDA())
+				{
+					Player main = Player.main;
+					if (main.GetPDA().isInUse)
+					{
+						main.GetPDA().ui.OpenTab(PDATab.Inventory);
+					}
+				}
+			}
+			if (Input.GetKeyDown(KeyCode.Alpha2))
+			{
+				if (!checkPDA())
+				{
+					Player main = Player.main;
+					if (main.GetPDA().isInUse)
+					{
+						main.GetPDA().ui.OpenTab(PDATab.Journal);
+					}
+				}
+			}
+			if (Input.GetKeyDown(KeyCode.Alpha3))
+			{
+				if (!checkPDA())
+				{
+					Player main = Player.main;
+					if (main.GetPDA().isInUse)
+					{
+						main.GetPDA().ui.OpenTab(PDATab.Ping);
+					}
+				}
+			}
+			if (Input.GetKeyDown(KeyCode.Alpha4))
+			{
+				if (!checkPDA())
+				{
+					Player main = Player.main;
+					if (main.GetPDA().isInUse)
+					{
+						main.GetPDA().ui.OpenTab(PDATab.Gallery);
+					}
+				}
+			}
+			if (Input.GetKeyDown(KeyCode.Alpha5))
+			{
+				if (!checkPDA())
+				{
+					Player main = Player.main;
+					if (main.GetPDA().isInUse)
+					{
+						main.GetPDA().ui.OpenTab(PDATab.Log);
+					}
+				}
+			}
+			if (Input.GetKeyDown(KeyCode.Alpha6))
+			{
+				if (!checkPDA())
+				{
+					Player main = Player.main;
+					if (main.GetPDA().isInUse)
+					{
+						main.GetPDA().ui.OpenTab(PDATab.Encyclopedia);
+					}
+				}
+			}
+		}
+	}
+
+}

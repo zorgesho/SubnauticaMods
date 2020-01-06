1a8511b249d78e923a06e2f851e9c4a16c539f3e
zorgesho <53096970+zorgesho@users.noreply.github.com>
2020-01-06 17:28:43 +0300

    new objects added: CargoBoxBig & HardenedBlade

---
+++ MiscObjects/src/buildables/CargoBoxBig.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using SMLHelper.V2.Crafting;
+
+using Common;
+using Common.Crafting;
+
+namespace MiscObjects
+{
+	class CargoBoxBig: CraftableObject
+	{
+		protected override TechData getTechData() => new TechData(new Ingredient(TechType.Titanium, 4));
+
+		public override void patch()
+		{
+			register("Big cargo box", "Cargo box for outside.", AssetsHelper.loadSprite(nameof(CargoBox)));
+
+			addToGroup(TechGroup.ExteriorModules, TechCategory.ExteriorOther, TechType.FarmingTray);
+			unlockOnStart();
+		}
+
+		public override GameObject getGameObject()
+		{
+			GameObject prefab = Object.Instantiate(Resources.Load<GameObject>("WorldEntities/Doodads/Debris/Wrecks/Decoration/Starship_cargo"));
+			GameObject model = prefab.FindChild("Starship_cargo");
+
+			prefab.transform.localScale *= 2.1f;
+			prefab.destroyComponent<Rigidbody>();
+
+			Constructable constructable = prefab.AddComponent<Constructable>().initDefault(model, TechType);
+			constructable.allowedOutside = true;
+			constructable.allowedOnGround = true;
+			constructable.allowedOnConstructables = true;
+			constructable.forceUpright = true;
+			constructable.placeDefaultDistance = 6f;
+
+			StorageHelper.addStorageToPrefab(prefab, L10n.str(L10n.ids_OpenBox), L10n.str(L10n.ids_BoxInv), 8, 8);
+			prefab.GetComponent<StorageContainer>().modelSizeRadius *= 3f;
+
+			return prefab;
+		}
+	}
+}
---
+++ MiscObjects/src/craftables/HardenedBlade.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+using SMLHelper.V2.Crafting;
+
+using Common.Crafting;
+
+namespace MiscObjects
+{
+	class DiamondBlade: CraftableObject
+	{
+		protected override TechData getTechData() => new TechData() { craftAmount = 1, Ingredients = new List<Ingredient>
+		{
+			new Ingredient(TechType.Knife, 1),
+			new Ingredient(TechType.Diamond, 2),
+		}};
+
+		public override GameObject getGameObject()
+		{
+			GameObject prefab = Object.Instantiate(Resources.Load<GameObject>("WorldEntities/Tools/DiamondBlade"));
+			prefab.GetComponent<Knife>().bleederDamage = 30f;
+
+			return prefab;
+		}
+
+		public override void patch()
+		{
+			register(TechType.DiamondBlade);
+
+			addToGroup(TechGroup.Workbench, TechCategory.Workbench, TechType.HeatBlade);
+			addCraftingNodeTo(CraftTree.Type.Workbench, "KnifeMenu");
+			setTechTypeForUnlock(TechType.Diamond);
+		}
+	}
+}

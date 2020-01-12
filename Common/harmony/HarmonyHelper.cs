594f223e909931c8ce2230f1502089197b198a30
zorgesho <53096970+zorgesho@users.noreply.github.com>
2020-01-13 02:25:34 +0300

    profiling added to 'patch' and 'patchAll'

--- Common/harmony/HarmonyHelper.cs
+++ Common/harmony/HarmonyHelper.cs
@@ -14,16 +14,21 @@ namespace Common
 				findConfig("Main", "config"); // need to be called before harmony patching
 
 			harmonyInstance = HarmonyInstance.Create(Strings.modName);
+
+			Debug.startStopwatch();
 			harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+			Debug.stopStopwatch($"Harmony.PatchAll {Strings.modName}");
 		}
 
 		public static void patch(MethodBase original, MethodInfo prefix = null, MethodInfo postfix = null, MethodInfo transpiler = null)
 		{																					$"HarmonyHelper.patch: patching '{original}' with prefix:'{prefix}' postfix:'{postfix}' transpiler:'{transpiler}'".logDbg();
 			try
 			{
+				Debug.startStopwatch();
 				harmonyInstance.Patch(original, (prefix == null)? null: new HarmonyMethod(prefix),
 												(postfix == null)? null: new HarmonyMethod(postfix),
 												(transpiler == null)? null: new HarmonyMethod(transpiler));
+				Debug.stopStopwatch($"HarmonyHelper.patch");
 			}
 			catch (System.Exception e)
 			{

7a66887a4180a9c12234cfb9124b76f52159d510
zorgesho <53096970+zorgesho@users.noreply.github.com>
2020-02-25 21:43:11 +0300

    .csproj files are cleaned up (references, post-build events, configurations)

---
+++ post-build.bat
@@ -0,0 +1,15 @@
+:: %1 $(ConfigurationName)
+:: %2 $(ProjectName)
+:: %3 $(ProjectDir)
+:: %4 $(TargetPath)
+:: %5 $(TargetDir)
+
+if %1 == test_build goto :exit
+set qmods_path=c:\games\subnautica\QMods
+if not exist %qmods_path% goto :exit
+
+xcopy %4 %qmods_path%\%2\ /q /y
+xcopy %3mod.json %qmods_path%\%2\ /q /y
+if exist %3assets\ xcopy %3assets %qmods_path%\%2\assets\ /e /q /y
+
+:exit

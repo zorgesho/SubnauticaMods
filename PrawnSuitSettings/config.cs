2c5765ae2bbedde820e77bf9c64e66ee1de3e049
zorgesho <53096970+zorgesho@users.noreply.github.com>
2020-03-25 01:32:11 +0300

    options update (sliders & tooltips)

--- PrawnSuitSettings/config.cs
+++ PrawnSuitSettings/config.cs
@@ -1,4 +1,5 @@
-﻿using Common.Configuration;
+﻿using Common;
+using Common.Configuration;
 
 namespace PrawnSuitSettings
 {
@@ -8,23 +9,68 @@ namespace PrawnSuitSettings
 	{
 		public class CollisionSelfDamageSettings
 		{
-			[Options.Field("Damage from collisions")]
+			class Hider: Field.ICustomAction, Options.Components.Hider.IVisibilityChecker
+			{
+				public bool isVisible() => Main.config.collisionSelfDamage.enabled;
+				public void customAction() => Options.Components.Hider.setVisible("collision", isVisible());
+			}
+
+			[Options.Field("Damage from collisions", "Damage for Prawn Suit from collisions with terrain and other objects")]
+			[HarmonyHelper.UpdatePatchesAction]
+			[Field.CustomAction(typeof(Hider))]
 			[Field.CustomAction(typeof(CollisionSelfDamage.SettingChanged))]
 			public readonly bool enabled = false;
 
+			[Options.Field("\tMinimum speed", "Prawn Suit minimum speed to get self damage from collision")]
+			[Field.Range(0f, 50f)]
+			[Options.Slider(DefaultValue: 20f)]
+			[Options.Hideable(typeof(Hider), "collision")]
+			[Field.CustomAction(typeof(CollisionSelfDamage.SettingChanged))]
 			public readonly float speedMinimumForDamage = 20f;
+
+			[Options.Field("\tMirrored damage fraction", "Fraction of total inflicted collision damage that goes to self damage")]
+			[Field.Range(0f, 1f)]
+			[Options.Slider(DefaultValue: 0.1f, ValueFormat: "{0:P0}")]
+			[Options.Hideable(typeof(Hider), "collision")]
+			[Field.CustomAction(typeof(CollisionSelfDamage.SettingChanged))]
 			public readonly float mirroredSelfDamageFraction = 0.1f;
 		}
 		public readonly CollisionSelfDamageSettings collisionSelfDamage = new CollisionSelfDamageSettings();
 
+
 		public class ArmsEnergyUsageSettings
 		{
-			[Options.Field("Arms additional energy usage")]
+			class Hider: Field.ICustomAction, Options.Components.Hider.IVisibilityChecker
+			{
+				public bool isVisible() => Main.config.armsEnergyUsage.enabled;
+				public void customAction() => Options.Components.Hider.setVisible("arms_energy", isVisible());
+			}
+
+			[Options.Field("Arms additional energy usage", "Energy consuming for drill arm and grappling arm")]
+			[HarmonyHelper.UpdatePatchesAction]
+			[Field.CustomAction(typeof(Hider))]
 			[Field.CustomAction(typeof(ArmsEnergyUsage.SettingChanged))]
 			public readonly bool enabled = false;
 
+			[Options.Field("\tDrill arm", "Using drill arm costs that much energy units per second")]
+			[Field.Range(0f, 5f)]
+			[Options.Slider(DefaultValue: 0.3f, ValueFormat: "{0:F1}")]
+			[Options.Hideable(typeof(Hider), "arms_energy")]
+			[Field.CustomAction(typeof(ArmsEnergyUsage.SettingChanged))]
 			public readonly float drillArm = 0.3f;
+
+			[Options.Field("\tGrappling arm (shoot)", "Shooting grappling hook costs that much energy units")]
+			[Field.Range(0f, 5f)]
+			[Options.Slider(DefaultValue: 0.5f, ValueFormat: "{0:F1}")]
+			[Options.Hideable(typeof(Hider), "arms_energy")]
+			[Field.CustomAction(typeof(ArmsEnergyUsage.SettingChanged))]
 			public readonly float grapplingArmShoot = 0.5f;
+
+			[Options.Field("\tGrappling arm (pull)", "Using grappling arm to pull Prawn Suit costs that much energy units per second")]
+			[Field.Range(0f, 5f)]
+			[Options.Slider(DefaultValue: 0.2f, ValueFormat: "{0:F1}")]
+			[Options.Hideable(typeof(Hider), "arms_energy")]
+			[Field.CustomAction(typeof(ArmsEnergyUsage.SettingChanged))]
 			public readonly float grapplingArmPull = 0.2f;
 
 			// vanilla energy usage
@@ -33,14 +79,16 @@ namespace PrawnSuitSettings
 		}
 		public readonly ArmsEnergyUsageSettings armsEnergyUsage = new ArmsEnergyUsageSettings();
 
-		[Options.Field("Propulsion arm 'ready' animation")]
+		[Options.Field("Propulsion arm 'ready' animation", "Whether propulsion arm should play animation when pointed to something pickupable")]
+		[HarmonyHelper.UpdatePatchesAction]
 		public readonly bool readyAnimationForPropulsionCannon = true;
 
-		[Options.Field("Toggleable drill arm")]
+		[Options.Field("Toggleable drill arm", "Whether you need to hold mouse button while using drill arm")]
+		[HarmonyHelper.UpdatePatchesAction]
 		public readonly bool toggleableDrillArm = false;
 
-		[Options.Field("Auto pickup resources after drilling")]
-		[Field.CustomAction(typeof(Common.HarmonyHelper.UpdateOptionalPatches))]
+		[Options.Field("Auto pickup resources after drilling", "Drilled resources will be added to the Prawn Suit storage automatically")]
+		[HarmonyHelper.UpdatePatchesAction]
 		public readonly bool autoPickupDrillableResources = true;
 	}
 }

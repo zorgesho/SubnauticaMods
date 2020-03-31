5c24719bc8028f7e3f5ad4589905cbb1081c5d74
zorgesho <53096970+zorgesho@users.noreply.github.com>
2020-04-01 00:38:40 +0300

    Mod class added (for initialization)

---
+++ Common/Common.Config/Mod.cs
@@ -0,0 +1,13 @@
+﻿using Common.Configuration;
+
+namespace Common
+{
+	static partial class Mod
+	{
+		public static C init<C>() where C: Config, new()
+		{
+			init();
+			return Config.tryLoad<C>();
+		}
+	}
+}
---
+++ Common/Mod.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using System.Globalization;
+
+namespace Common
+{
+	static partial class Mod
+	{
+		// supposed to be called before any other mod's code
+		public static void init()
+		{
+			// may be overkill to make it for all mods and from the start
+			Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+			"Mod inited".logDbg();
+		}
+	}
+}

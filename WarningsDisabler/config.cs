2d4f0db589c442ace8a8972a1a02beb1cdb2a443
zorgesho <53096970+zorgesho@users.noreply.github.com>
2020-04-04 15:43:33 +0300

    WarningsDisabler 1.2.0

--- WarningsDisabler/config.cs
+++ WarningsDisabler/config.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Reflection;
 using System.Collections.Generic;
 
@@ -7,7 +8,7 @@ using Common.Configuration;
 
 namespace WarningsDisabler
 {
-	[Options.Name("Warnings & messages <color=#CCCCCCFF>(uncheck boxes to disable)</color>")]
+	[Options.Name("Warnings & messages")]
 	class ModConfig: Config
 	{
 		class Messages
@@ -15,32 +16,83 @@ namespace WarningsDisabler
 			public bool enabled = true;
 
 			[NoInnerFieldsAttrProcessing]
-			readonly HashSet<string> messages = null;
+			public readonly HashSet<string> messages; // hashset to avoid duplicating string while loading
 
-			public Messages(params string[] _messages) => messages = new HashSet<string>(_messages);
+			public Messages(params string[] messages) => this.messages = new HashSet<string>(messages);
 
-			public bool isEmpty() => messages.Count == 0;
 			public bool isMessageAllowed(string message) => enabled || !messages.Contains(message);
 		}
+
+
+		class MessageListTooltip: Options.Components.TooltipCached<bool>
+		{
+			protected Messages msgList;
+
+			protected virtual void Awake()
+			{
+				msgList = parentOption.cfgField.parentObject as Messages;
+				Debug.assert(msgList != null);
+			}
+
+			protected override bool needUpdate => isParamsChanged(msgList.enabled);
+
+			class L10n: LanguageHelper
+			{
+				public static readonly string ids_thisMessage  = "This message is {0}:";
+				public static readonly string ids_thisMessages = "This messages are {0}:";
+				public static readonly string ids_enabled  = "<color=#00ff00ff>enabled</color>";
+				public static readonly string ids_disabled = "<color=#ffff00ff>disabled</color>";
+			}
+
+			public override string tooltip
+			{
+				get
+				{
+					StringBuilder sb = new StringBuilder();
+
+					string title = L10n.str(msgList.messages.Count == 1? "ids_thisMessage": "ids_thisMessages");
+					title = string.Format(title, L10n.str(msgList.enabled? "ids_enabled": "ids_disabled"));
+
+					sb.AppendLine("<size=20><color=#ffffffff>" + title + "</color></size>");
+
+					sb.Append("<size=19>");
+					int i = 0;
+					foreach (var msg in msgList.messages)
+					{
+						sb.Append("\"" + Language.main.Get(msg) + "\"");
+						if (i++ != msgList.messages.Count - 1)
+							sb.AppendLine();
+					}
+					sb.Append("</size>");
+
+					return sb.ToString();
+				}
+			}
+		}
+
 
 		class AddMessagesAttribute: Attribute, IFieldAttribute
 		{
 			readonly string label;
 
-			public AddMessagesAttribute(string _label) => label = _label;
+			public AddMessagesAttribute(string label) => this.label = label;
 
 			public void process(object config, FieldInfo field)
 			{
 				Messages messages = field.GetValue(config) as Messages;
 				Debug.assert(messages != null);
 
-				if (!messages.isEmpty())
-				{
-					var cfgField = new Field(messages, nameof(Messages.enabled));
-					Options.add(new Options.ToggleOption(cfgField, label));
+				if (messages.messages.Count == 0)
+					return;
 
-					(Config.main as ModConfig).allMessages.Add(messages);
-				}
+				var cfgField = new Field(messages, nameof(Messages.enabled));
+
+				var option = new Options.ToggleOption(cfgField, label);
+				option.addHandler(new Options.Components.Tooltip.Add(typeof(MessageListTooltip), null));
+
+				Options.add(option);
+
+				(Config.main as ModConfig).allMessages.Add(messages);
 			}
 		}
 
@@ -51,22 +103,31 @@ namespace WarningsDisabler
 		public bool isMessageAllowed(string message) => !allMessages.Exists(list => !list.isMessageAllowed(message));
 
 
-		[Options.Field("Oxygen warnings")]
-		[Field.Action(typeof(OxygenWarnings.HideOxygenHint))]
+		[Options.Field("Oxygen warnings", tooltipType: typeof(OxygenTooltip))]
+		[Options.FinalizeAction(typeof(OxygenWarnings.HideOxygenHint))]
 		public readonly bool oxygenWarningsEnabled = true;
+
+		class OxygenTooltip: MessageListTooltip
+		{
+			protected override void Awake() =>
+				msgList = new Messages("OxygenWarning10", "OxygenWarning30");
+
+			protected override bool needUpdate =>
+				isParamsChanged(msgList.enabled = Main.config.oxygenWarningsEnabled);
+		}
 
 #pragma warning disable IDE0052 // field is never read
 
 		[AddMessages("Food and water warnings")]
 		readonly Messages foodWaterWarnings = new Messages
 		(
-			"VitalsOk",			// "Vital signs stabilizing."
 			"FoodLow",			// "Calorie intake recommended."
-			"FoodCritical",		// "Emergency, starvation imminent. Seek calorie intake immediately."
 			"FoodVeryLow",		// "Seek calorie intake."
+			"FoodCritical",		// "Emergency, starvation imminent. Seek calorie intake immediately."
 			"WaterLow",			// "Seek fluid intake."
+			"WaterVeryLow",		// "Seek fluid intake."
 			"WaterCritical",	// "Seek fluid intake immediately."
-			"WaterVeryLow"		// "Seek fluid intake."
+			"VitalsOk"			// "Vital signs stabilizing."
 		);
 
 		[AddMessages("Depth warnings")]
@@ -79,9 +140,9 @@ namespace WarningsDisabler
 		[AddMessages("Habitat power warnings")]
 		readonly Messages powerWarnings = new Messages
 		(
-			"BasePowerUp",			// "HABITAT: Power restored. All primary systems online."
 			"BasePowerDown",		// "HABITAT: Warning, emergency power only."
-			"BaseWelcomeNoPower"	// "HABITAT: Warning: Emergency power only. Oxygen production offline."
+			"BaseWelcomeNoPower",	// "HABITAT: Warning: Emergency power only. Oxygen production offline."
+			"BasePowerUp"			// "HABITAT: Power restored. All primary systems online."
 		);
 
 		[AddMessages("Welcome messages")]
--- WarningsDisabler/main.cs
+++ WarningsDisabler/main.cs
@@ -1,15 +1,16 @@
 ﻿using Common;
-using Common.Configuration;
 
 namespace WarningsDisabler
 {
 	public static class Main
 	{
-		internal static readonly ModConfig config = Config.tryLoad<ModConfig>();
+		internal static readonly ModConfig config = Mod.init<ModConfig>();
 
 		public static void patch()
 		{
 			HarmonyHelper.patchAll();
+
+			LanguageHelper.init();
 		}
 	}
 }

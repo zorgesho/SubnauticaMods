c5bba328936f542d5632801ea99a3b679fd48d8a
zorgesho <53096970+zorgesho@users.noreply.github.com>
2020-04-11 16:45:34 +0300

    PrawnSuitGrapplingArmUpgrade 1.1.0

--- PrawnSuitGrapplingArmUpgrade/properties/AssemblyInfo.cs
+++ PrawnSuitGrapplingArmUpgrade/properties/AssemblyInfo.cs
@@ -2,15 +2,9 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("PrawnSuitGrapplingArmUpgrade")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PrawnSuitGrapplingArmUpgrade")]
-[assembly: AssemblyCopyright("© 2019 zorgesho")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("© 2019-2020 zorgesho")]
 
 [assembly: ComVisible(false)]
-[assembly: Guid("2518edf1-782e-4faa-b93d-4f9991962144")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
--- PrawnSuitGrapplingArmUpgrade/src/ArmModule.cs
+++ PrawnSuitGrapplingArmUpgrade/src/ArmModule.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-using UnityEngine;
+﻿using UnityEngine;
 using SMLHelper.V2.Crafting;
 
 using Common.Crafting;
@@ -13,16 +11,23 @@ namespace PrawnSuitGrapplingArmUpgrade
 	{
 		public static new TechType TechType { get; private set; } = 0;
 
-		protected override TechData getTechData() => new TechData() { craftAmount = 1, Ingredients = new List<Ingredient>
-		{
+		protected override TechData getTechData() => new TechData
+		(
 			new Ingredient(TechType.ExosuitGrapplingArmModule, 1),
 			new Ingredient(TechType.Polyaniline, 2),
 			new Ingredient(TechType.Lithium, 2),
 			new Ingredient(TechType.AramidFibers, 1),
-			new Ingredient(TechType.AluminumOxide, 1),
-		}};
+			new Ingredient(TechType.AluminumOxide, 1)
+		)	{ craftAmount = 1};
 
-		public override GameObject getGameObject() => Object.Instantiate(CraftData.GetPrefabForTechType(TechType.ExosuitGrapplingArmModule));
+		public override GameObject getGameObject()
+		{
+			var prefab = CraftHelper.Utils.prefabCopy(TechType.ExosuitGrapplingArmModule);
+
+			CraftHelper.Utils.initVFXFab(prefab, posOffset: new Vector3(0.08f, 0.1f, 0f), localMaxY: 0.35f, scaleFactor: 0.5f);
+
+			return prefab;
+		}
 
 		public override void patch()
 		{

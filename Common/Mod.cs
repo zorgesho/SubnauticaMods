c9bfa5933b12d6a8deaf77b47d0703e71bd56713
zorgesho <53096970+zorgesho@users.noreply.github.com>
2020-04-12 15:51:30 +0300

    .

--- Common/Mod.cs
+++ Common/Mod.cs
@@ -1,15 +1,22 @@
-﻿using System.Threading;
+﻿using System;
+using System.IO;
+using System.Threading;
 using System.Globalization;
 
 namespace Common
 {
 	static partial class Mod
 	{
+		const string tmpFileName = "run the game to generate configs"; // name is also in the post-build.bat
+
 		// supposed to be called before any other mod's code
 		public static void init()
 		{
 			// may be overkill to make it for all mods and from the start
 			Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+			try { File.Delete(Paths.modRootPath + tmpFileName); }
+			catch (UnauthorizedAccessException) {}
 
 			"Mod inited".logDbg();
 		}
--- post-build.bat
+++ post-build.bat
@@ -4,12 +4,18 @@
 :: %4 $(TargetPath)
 :: %5 $(TargetDir)
 
+set qmods_path="c:\games\subnautica\QMods"
+
+:: name is also in the Mod.cs
+set tmp_info_file="run the game to generate configs"
+
 if %1 == test_build goto :exit
-set qmods_path=c:\games\subnautica\QMods
 if not exist %qmods_path% goto :exit
 
 xcopy %4 %qmods_path%\%2\ /q /y
 xcopy %3mod.json %qmods_path%\%2\ /q /y
 if exist %3assets\ xcopy %3assets %qmods_path%\%2\assets\ /e /q /y
+
+if %1 == publish copy nul %qmods_path%\%2\%tmp_info_file%
 
 :exit

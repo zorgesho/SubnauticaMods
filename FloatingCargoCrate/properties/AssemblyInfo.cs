f6b6ef7e61655370c52c7a54dcf0c04462ddb9b9
zorgesho <53096970+zorgesho@users.noreply.github.com>
2020-04-16 21:56:45 +0300

    FloatingCargoCrate 1.1.2

--- FloatingCargoCrate/properties/AssemblyInfo.cs
+++ FloatingCargoCrate/properties/AssemblyInfo.cs
@@ -2,15 +2,9 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("FloatingCargoCrate")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FloatingCargoCrate")]
-[assembly: AssemblyCopyright("© 2019 zorgesho")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("© 2019-2020 zorgesho")]
 
 [assembly: ComVisible(false)]
-[assembly: Guid("9e3d5f87-c19e-482b-ac15-7e56deed70fa")]
 
-[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.1.2.0")]

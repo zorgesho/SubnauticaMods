82366e420587154a54f723af37b2d80f4c53cea8
zorgesho <53096970+zorgesho@users.noreply.github.com>
2020-05-20 06:24:03 +0300

    .

--- Common/MainMenuMessages.cs
+++ Common/MainMenuMessages.cs
@@ -11,102 +11,105 @@ namespace Common
 	using Harmony;
 	using Reflection;
 
-	static class MainMenuMessages
+	static partial class Mod
 	{
-		public const int defaultSize = 25;
-		public const string defaultColor = "red";
-
-		static List<string> messageQueue;
-		static List<ErrorMessage._Message> messages;
-
-		public static void add(string msg, int size = defaultSize, string color = defaultColor, bool autoformat = true)
+		static class MainMenuMessages
 		{
-			if (autoformat)
-				msg = $"<size={size}><color={color}><b>[{Mod.name}]:</b> {msg}</color></size>";
+			public const int defaultSize = 25;
+			public const string defaultColor = "red";
 
-			init();
+			static List<string> messageQueue;
+			static List<ErrorMessage._Message> messages;
 
-			if (ErrorMessage.main != null)
-				_add(msg);
-			else
-				messageQueue.Add(msg);
-		}
-
-		static void init()
-		{
-			if (messageQueue != null)
-				return;
-
-			messageQueue = new List<string>();
-			messages = new List<ErrorMessage._Message>();
-			Patches.patch();
-
-			SceneManager.sceneLoaded += onSceneLoaded;
-		}
-
-		static void _add(string msg)
-		{
-			ErrorMessage.AddDebug(msg);
-
-			var message = ErrorMessage.main.GetExistingMessage(msg);
-			messages.Add(message);
-			message.timeEnd += 1e6f;
-		}
-
-		static void onSceneLoaded(Scene scene, LoadSceneMode _)
-		{
-			if (scene.name != "Main")
-				return;
-																						"MainMenuMessages: game loading started".logDbg();
-			SceneManager.sceneLoaded -= onSceneLoaded;
-			ErrorMessage.main.StartCoroutine(_waitForLoad());
-
-			static IEnumerator _waitForLoad()
+			public static void add(string msg, int size = defaultSize, string color = defaultColor, bool autoformat = true)
 			{
-				yield return new WaitForSeconds(1f);
+				if (autoformat)
+					msg = $"<size={size}><color={color}><b>[{Mod.name}]:</b> {msg}</color></size>";
 
-				while (SaveLoadManager.main.isLoading)
-					yield return null;
-																						"MainMenuMessages: game loading finished".logDbg();
-				messages.ForEach(msg => msg.timeEnd = Time.time + 1f);
+				init();
 
-				messages.Clear();
-				Patches.unpatch();
-			}
-		}
-
-		static class Patches
-		{
-			public static void patch()
-			{
-				HarmonyHelper.patch();
+				if (ErrorMessage.main != null)
+					_add(msg);
+				else
+					messageQueue.Add(msg);
 			}
 
-			public static void unpatch()
+			static void init()
 			{
-				HarmonyHelper.unpatch(typeof(ErrorMessage).method("Awake"), typeof(Patches).method(nameof(addMessages)));
-				HarmonyHelper.unpatch(typeof(ErrorMessage).method("OnUpdate"), typeof(Patches).method(nameof(updateMessages)));
+				if (messageQueue != null)
+					return;
+
+				messageQueue = new List<string>();
+				messages = new List<ErrorMessage._Message>();
+				Patches.patch();
+
+				SceneManager.sceneLoaded += onSceneLoaded;
 			}
 
-			[HarmonyPostfix]
-			[HarmonyPatch(typeof(ErrorMessage), "Awake")]
-			static void addMessages()
+			static void _add(string msg)
 			{
-				messageQueue.ForEach(msg => _add(msg));
-				messageQueue.Clear();
+				ErrorMessage.AddDebug(msg);
+
+				var message = ErrorMessage.main.GetExistingMessage(msg);
+				messages.Add(message);
+				message.timeEnd += 1e6f;
 			}
 
-			static float _getVal(float val, ErrorMessage._Message message) => messages.Contains(message)? 1f: val;
-
-			// we changing result for 'float value = Mathf.Clamp01(MathExtensions.EvaluateLine(...' to 1.0f
-			// so text don't stay in the center of the screen (because of changed 'timeEnd')
-			[HarmonyTranspiler]
-			[HarmonyPatch(typeof(ErrorMessage), "OnUpdate")]
-			static IEnumerable<CodeInstruction> updateMessages(IEnumerable<CodeInstruction> cins)
+			static void onSceneLoaded(Scene scene, LoadSceneMode _)
 			{
-				return CIHelper.ciInsert(cins, cin => cin.isOpLoc(OpCodes.Stloc_S, 11), +0, 1,
-						OpCodes.Ldloc_S, 6,
-						OpCodes.Call, typeof(Patches).method(nameof(_getVal)));
+				if (scene.name != "Main")
+					return;
+																							"MainMenuMessages: game loading started".logDbg();
+				SceneManager.sceneLoaded -= onSceneLoaded;
+				ErrorMessage.main.StartCoroutine(_waitForLoad());
+
+				static IEnumerator _waitForLoad()
+				{
+					yield return new WaitForSeconds(1f);
+
+					while (SaveLoadManager.main.isLoading)
+						yield return null;
+																							"MainMenuMessages: game loading finished".logDbg();
+					messages.ForEach(msg => msg.timeEnd = Time.time + 1f);
+
+					messages.Clear();
+					Patches.unpatch();
+				}
+			}
+
+			static class Patches
+			{
+				public static void patch()
+				{
+					HarmonyHelper.patch();
+				}
+
+				public static void unpatch()
+				{
+					HarmonyHelper.unpatch(typeof(ErrorMessage).method("Awake"), typeof(Patches).method(nameof(addMessages)));
+					HarmonyHelper.unpatch(typeof(ErrorMessage).method("OnUpdate"), typeof(Patches).method(nameof(updateMessages)));
+				}
+
+				[HarmonyPostfix]
+				[HarmonyPatch(typeof(ErrorMessage), "Awake")]
+				static void addMessages()
+				{
+					messageQueue.ForEach(msg => _add(msg));
+					messageQueue.Clear();
+				}
+
+				static float _getVal(float val, ErrorMessage._Message message) => messages.Contains(message)? 1f: val;
+
+				// we changing result for 'float value = Mathf.Clamp01(MathExtensions.EvaluateLine(...' to 1.0f
+				// so text don't stay in the center of the screen (because of changed 'timeEnd')
+				[HarmonyTranspiler]
+				[HarmonyPatch(typeof(ErrorMessage), "OnUpdate")]
+				static IEnumerable<CodeInstruction> updateMessages(IEnumerable<CodeInstruction> cins)
+				{
+					return CIHelper.ciInsert(cins, cin => cin.isOpLoc(OpCodes.Stloc_S, 11), +0, 1,
+							OpCodes.Ldloc_S, 6,
+							OpCodes.Call, typeof(Patches).method(nameof(_getVal)));
+				}
 			}
 		}
 	}

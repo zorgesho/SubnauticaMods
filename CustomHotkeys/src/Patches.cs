a8f010104ca7b536434e9add83343eb97a35d55e
zorgesho <53096970+zorgesho@users.noreply.github.com>
2020-05-28 19:10:41 +0300

    fix for 'Up' event after binding

--- CustomHotkeys/src/Patches.cs
+++ CustomHotkeys/src/Patches.cs
@@ -48,9 +48,8 @@ namespace CustomHotkeys
 		static void Postfix(uGUI_FeedbackCollector __instance) => __instance.enabled = Main.config.enableFeedback;
 	}
 
-	// allows to remove bindings from bind options without selecting them first
-	// it's enough to just move cursor over the option and press 'Delete'
-	static class BindRemover
+	// patches for removing bindings and blocking 'Up' event after binding
+	static class BindingPatches
 	{
 		class BindCheckPointer: MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 		{
@@ -60,6 +59,9 @@ namespace CustomHotkeys
 			public void OnPointerExit(PointerEventData eventData)  => hoveredObject = null;
 		}
 
+		// allows to remove bindings from bind options without selecting them first
+		// it's enough to just move cursor over the option and press 'Delete'
+		// another part of the patch in the 'Update' patch
 		[HarmonyPatch(typeof(uGUI_Binding), "Start")]
 		static class uGUIBinding_Start_Patch
 		{
@@ -69,22 +71,64 @@ namespace CustomHotkeys
 				__instance.gameObject.ensureComponent<BindCheckPointer>();
 		}
 
+		static int lastBindedIndex = -1;
+
 		[HarmonyPatch(typeof(uGUI_Binding), "Update")]
 		static class uGUIBinding_Update_Patch
 		{
-			static bool Prepare() => Main.config.easyBindRemove;
+			static void saveLastBind() => lastBindedIndex = GameInput.lastInputPressed[0]; // for keyboard
 
 			static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> cins)
 			{
 				var list = cins.ToList();
 
-				var toReplace = typeof(EventSystem).method("get_current");
-				int index = list.FindIndex(ci => ci.isOp(OpCodes.Call, toReplace));
+				// saving binded keycode to check later in GameInput.UpdateKeyInputs patch
+				var GameInput_ClearInput = typeof(GameInput).method("ClearInput");
+				CIHelper.ciInsert(list, ci => ci.isOp(OpCodes.Call, GameInput_ClearInput),
+					OpCodes.Call, typeof(uGUIBinding_Update_Patch).method(nameof(saveLastBind)));
+
+				if (Main.config.easyBindRemove)
+				{
+					var toReplace = typeof(EventSystem).method("get_current");
+					int index = list.FindIndex(ci => ci.isOp(OpCodes.Call, toReplace));
+					Debug.assert(index != -1);
+
+					var get_hoveredObject = typeof(BindCheckPointer).method("get_" + nameof(BindCheckPointer.hoveredObject));
+					list.RemoveRange(index, 2);
+					list.Insert(index, new CodeInstruction(OpCodes.Call, get_hoveredObject));
+				}
+
+				return list;
+			}
+		}
+
+		// if we press key while binding in options menu, ignore its 'Up' event too
+		[HarmonyPatch(typeof(GameInput), "UpdateKeyInputs")]
+		static class GameInput_UpdateKeyInputs_Patch
+		{
+			static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> cins, ILGenerator ilg)
+			{
+				var list = cins.ToList();
+
+				var Input_GetKeyUp = typeof(Input).method("GetKeyUp", typeof(KeyCode));
+				int index = list.FindIndex(ci => ci.isOp(OpCodes.Call, Input_GetKeyUp));
 				Debug.assert(index != -1);
 
-				CIHelper.ciRemove(list, index, 2);
-				CIHelper.ciInsert(list, index,
-					CIHelper.toCIList(OpCodes.Call, typeof(BindCheckPointer).method("get_" + nameof(BindCheckPointer.hoveredObject))));
+				Label lb1 = list[index + 1].operand.cast<Label>();
+				Label lb2 = ilg.DefineLabel();
+				list[index + 2].labels.Add(lb2); // label for 'inputState.flags |= GameInput.InputStateFlags.Up'
+
+				var field_lastBindedIndex = typeof(BindingPatches).field(nameof(lastBindedIndex));
+
+				list.InsertRange(index + 2, CIHelper.toCIList
+				(
+					OpCodes.Ldloc_S, 4,						//	if (i == BindingPatches.lastBindedIndex)
+					OpCodes.Ldsfld, field_lastBindedIndex,
+					OpCodes.Bne_Un_S, lb2,
+					OpCodes.Ldc_I4_M1,						//		BindingPatches.lastBindedIndex = -1;
+					OpCodes.Stsfld, field_lastBindedIndex,
+					OpCodes.Br_S, lb1						//	else inputState.flags |= GameInput.InputStateFlags.Up;
+				));
 
 				return list;
 			}

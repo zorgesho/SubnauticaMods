bafa54f23a2e25031562db725d44be3543c2325a
zorgesho <53096970+zorgesho@users.noreply.github.com>
2020-05-29 15:17:38 +0300

    misc temporary and debug stuff

---
+++ MiscPrototypes/src/BasePower.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+
+using Harmony;
+
+using Common;
+
+namespace MiscPrototypes
+{
+	[HarmonyPatch(typeof(SubRoot), "Update")]
+	static class SubRoot_Update_Patch
+	{
+		static void Prefix(SubRoot __instance)
+		{
+			"update".onScreen("sub root update");
+
+			if (__instance is BaseRoot baseRoot)
+				baseRoot.ConsumePower();
+		}
+	}
+
+
+	[HarmonyPatch(typeof(BaseRoot), "ConsumePower")]
+	static class BaseRoot_ConsumePower_Patch
+	{
+		static bool Prefix(BaseRoot __instance)
+		{
+			if (!__instance.powerRelay)
+				return false;
+
+			"----------------- consuming begin".log();
+
+			StringBuilder sb = new StringBuilder();
+			//return Base.CellPowerConsumption[(int)this.GetCell(cell)];
+
+			float num = 0f;
+			Int3.RangeEnumerator allCells = __instance.baseComp.AllCells;
+			while (allCells.MoveNext())
+			{
+				Int3 cell = allCells.Current;
+				//$"cell: {cell}".log();
+				if (__instance.baseComp.GetCellMask(cell))
+				{
+					float cons = __instance.baseComp.GetCellPowerConsumption(cell);
+					num += cons;
+					if (cons > 0f)
+						sb.AppendLine($"cell: {cell} ({__instance.baseComp.GetCell(cell)}) power: {cons}");
+					//$"num added {num}".log();
+				}
+			}
+			num *= 1f;
+			DayNightCycle main = DayNightCycle.main;
+			if (main)
+			{
+				num *= main.deltaTime;
+			}
+			float num2;
+			__instance.powerRelay.ConsumeEnergy(num, out num2);
+
+			$"consuming {num}".onScreen("base root power");
+			sb.ToString().onScreen("cells power");
+
+			$"consuming {num} {UnityEngine.Time.frameCount}".log();
+
+			return false;
+		}
+	}
+
+	[HarmonyPatch(typeof(BaseRoot), "Start")]
+	static class BaseRoot_Start_Patch
+	{
+		static void Prefix(BaseRoot __instance)
+		{
+			"---------------- START!!!".log();
+		}
+	}
+}
---
+++ MiscPrototypes/src/ConsoleCommands.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+using Harmony;
+using UnityEngine;
+
+using Common;
+using Common.Reflection;
+
+namespace MiscPrototypes
+{
+	[HarmonyPatch(typeof(GameInput), "Initialize")] // just to create console commands object
+	static class GameInput_Awake_Patch_ConsoleCommands
+	{
+#pragma warning disable IDE0052
+		static GameObject go = null;
+#pragma warning restore
+		static void Postfix() => go ??= PersistentConsoleCommands.createGameObject<TestConsoleCommands>();
+	}
+
+
+	class TestConsoleCommands: PersistentConsoleCommands
+	{
+		void OnConsoleCommand_debug_gameinput(NotificationCenter.Notification n)
+		{
+			StopAllCoroutines();
+
+			if (n.getArg<bool>(0))
+				StartCoroutine(_dbg());
+
+			static IEnumerator _dbg()
+			{
+				while (true)
+				{
+					var sb = new StringBuilder();
+					sb.AppendLine();
+					sb.AppendLine($"clearInput: {GameInput.clearInput}; scanningInput: {GameInput.scanningInput}");
+					sb.AppendLine($"movedir: {GameInput.GetMoveDirection()}");
+					sb.AppendLine($"playercontroller: {Player.main?.playerController.inputEnabled}");
+					sb.AppendLine($"fpsmod lock: {FPSInputModule.current.lockMovement}");
+
+					foreach (var mod in InputHelper.KeyWithModifier.modifiers)
+					{
+						sb.AppendLine($"{mod}: down: {Input.GetKeyDown(mod)} up: {Input.GetKeyUp(mod)} held: {Input.GetKey(mod)}");
+					}
+
+					sb.ToString().onScreen("gameinput");
+					yield return null;
+				}
+			}
+		}
+
+
+		void OnConsoleCommand_debug_print_gameinput(NotificationCenter.Notification n)
+		{
+			var sb = new StringBuilder();
+			sb.AppendLine();
+			for (int i = 0; i < GameInput.inputs.Count; i++)
+			{
+				var input = GameInput.inputs[i];
+				sb.AppendLine($"{input.name} {(int)input.keyCode} {input.axis} {input.axisPositive}");
+			}
+
+			sb.ToString().saveToFile("gameinput");
+		}
+
+
+		void OnConsoleCommand_equip1(NotificationCenter.Notification n)
+		{
+			//this.DeselectInternal();
+			if (n.getArgCount() == 0)
+				return;
+
+			TechType techType = Enum.Parse(typeof(TechType), n.getArg(0), true).cast<TechType>();
+
+			if (techType == TechType.None)
+			{
+				"NONE".onScreen();
+				return;
+			}
+
+			InventoryItem inventoryItem = Inventory.main.container.GetItems(techType)[0];
+			if (inventoryItem != null)
+			{
+				Pickupable item = inventoryItem.item;
+				PlayerTool component = item.GetComponent<PlayerTool>();
+				if (component != null)
+				{
+					Inventory.main.quickSlots.DrawAsTool(component);
+				}
+				else
+				{
+					Inventory.main.quickSlots.DrawAsItem(inventoryItem);
+				}
+				Inventory.main.quickSlots._heldItem = inventoryItem;
+				//Inventory.main.quickSlots.NotifyToggle(slotID, true);
+				//this.NotifySelect(slotID);
+				//Equipment.SendEquipmentEvent(item, 0, this.owner, QuickSlots.slotNames[slotID]);
+			}
+		}
+
+		//new Hotkey { command = "item flashlight; item lasercutter", label = null /*"Toggle Fast Start"*/, key = KeyCode.R },
+		//new Hotkey { command = "bindslot 0 flashlight; equipslot 0", label = null /*"Toggle Fast Start"*/, key = KeyCode.F },
+		//new Hotkey { command = "bindslot 0 lasercutter; equipslot 0", label = null /*"Toggle Fast Start"*/, key = KeyCode.L },
+
+		void OnConsoleCommand_bindslot(NotificationCenter.Notification n)
+		{
+			if (n.getArgCount() < 2)
+				return;
+
+			TechType techType = Enum.Parse(typeof(TechType), n.getArg(1), true).cast<TechType>(); // TODO try convert
+
+			if (techType == TechType.None)
+			{
+				"NONE".onScreen();
+				return;
+			}
+
+			InventoryItem inventoryItem = Inventory.main.container.GetItems(techType)[0];
+			Inventory.main.quickSlots.Bind(n.getArg<int>(0), inventoryItem);
+		}
+
+		void OnConsoleCommand_equipslot(NotificationCenter.Notification n)
+		{
+			if (n.getArgCount() == 1)
+				Inventory.main.quickSlots.SlotKeyDown(n.getArg<int>(0));
+		}
+	}
+}

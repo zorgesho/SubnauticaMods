4e54ae0fff9aa726710636ebe709db9b2d574c12
zorgesho <53096970+zorgesho@users.noreply.github.com>
2020-07-05 13:51:41 +0300

    asset bundles support added

--- Common/Common.AssetsHelper/AssetsHelper.cs
+++ Common/Common.AssetsHelper/AssetsHelper.cs
@@ -1,10 +1,35 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using UnityEngine;
 
 namespace Common
 {
+	using Reflection;
+
 	static class AssetsHelper
 	{
+		// bundle should be placed and named like this - '{ModRoot}\assets\{ModID}.assets'
+		static readonly object assetBundle;
+
+		static AssetsHelper()
+		{
+			string bundlePath = Paths.assetsPath + Mod.id + ".assets";
+
+			if (File.Exists(bundlePath))
+			{
+				MethodWrapper loadBundle = Type.GetType("UnityEngine.AssetBundle, UnityEngine.AssetBundleModule").method("LoadFromFile", new [] {typeof(string)}).wrap();
+				assetBundle = loadBundle.invoke(bundlePath);
+			}
+		}
+
+		static MethodWrapper loadAsset;
+
+		public static GameObject loadPrefab(string prefabName)
+		{
+			loadAsset ??= Type.GetType("UnityEngine.AssetBundle, UnityEngine.AssetBundleModule").method("LoadAsset", new [] {typeof(string), typeof(Type)}).wrap();
+			return loadAsset.invoke(assetBundle, prefabName, typeof(GameObject)) as GameObject;
+		}
+
 		public static Sprite loadSprite(string textureName) =>
 			textureToSprite(loadTextureFromFile(Paths.assetsPath + textureName + ".png"));
 

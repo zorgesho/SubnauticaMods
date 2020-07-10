bc263cac60b0423b545be9681feb20654227e9f6
zorgesho <53096970+zorgesho@users.noreply.github.com>
2020-07-10 16:15:49 +0300

    loading textures from bundle

--- Common/Common.AssetsHelper/AssetsHelper.cs
+++ Common/Common.AssetsHelper/AssetsHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+
 using UnityEngine;
 
 namespace Common
@@ -8,6 +9,13 @@ namespace Common
 
 	static class AssetsHelper
 	{
+		public static Sprite loadSprite(string textureName) => textureToSprite(loadTexture(textureName));
+
+		public static Texture2D loadTexture(string textureName) => loadAsset<Texture2D>(textureName) ?? loadTextureFromFile(Paths.assetsPath + textureName);
+
+		public static GameObject loadPrefab(string prefabName) => loadAsset<GameObject>(prefabName);
+
+
 		// bundle should be placed and named like this - '{ModRoot}\assets\{ModID}.assets'
 		static readonly object assetBundle;
 
@@ -17,27 +25,32 @@ namespace Common
 
 			if (File.Exists(bundlePath))
 			{
-				MethodWrapper loadBundle = Type.GetType("UnityEngine.AssetBundle, UnityEngine.AssetBundleModule").method("LoadFromFile", new [] {typeof(string)}).wrap();
+				MethodWrapper loadBundle = Type.GetType("UnityEngine.AssetBundle, UnityEngine.AssetBundleModule").method("LoadFromFile", new[] { typeof(string) }).wrap();
 				assetBundle = loadBundle.invoke(bundlePath);
 			}
 		}
 
-		static MethodWrapper loadAsset;
+		static MethodWrapper _loadAsset;
 
-		public static GameObject loadPrefab(string prefabName)
-		{
-			loadAsset ??= Type.GetType("UnityEngine.AssetBundle, UnityEngine.AssetBundleModule").method("LoadAsset", new [] {typeof(string), typeof(Type)}).wrap();
-			return loadAsset.invoke(assetBundle, prefabName, typeof(GameObject)) as GameObject;
+		static T loadAsset<T>(string name) where T: UnityEngine.Object
+		{																								$"AssetHelper: trying to load asset '{name}' ({typeof(T)}) from bundle".logDbg();
+			if (assetBundle == null)
+				return null;
+
+			_loadAsset ??= Type.GetType("UnityEngine.AssetBundle, UnityEngine.AssetBundleModule").method("LoadAsset", new[] { typeof(string), typeof(Type) }).wrap();
+			return _loadAsset.invoke(assetBundle, name, typeof(T)) as T;
 		}
-
-		public static Sprite loadSprite(string textureName) =>
-			textureToSprite(loadTextureFromFile(Paths.assetsPath + textureName + ".png"));
 
 		static Sprite textureToSprite(Texture2D tex) =>
 			tex == null? null: Sprite.Create(tex, new Rect(0f, 0f, tex.width, tex.height), new Vector2(0.5f, 0.5f));
 
 		static Texture2D loadTextureFromFile(string textureFilePath)
-		{
+		{																								$"AssetHelper: trying to load texture from file '{textureFilePath}'".logDbg();
+			if (Path.GetExtension(textureFilePath) == "")
+			{
+				textureFilePath += ".png";																$"AssetHelper: adding extension to filename ({textureFilePath})'".logDbg();
+			}
+
 			if (!File.Exists(textureFilePath))
 				return null;
 

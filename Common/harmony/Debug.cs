79f3bbbffcfe3e899550235a0c1d395cc613dcdb
zorgesho <53096970+zorgesho@users.noreply.github.com>
2020-08-10 22:40:40 +0300

    CodeInstructions logging refactored instructions list can be saved to the separate file now

--- Common/harmony/Debug.cs
+++ Common/harmony/Debug.cs
@@ -16,20 +16,23 @@ namespace Common.Harmony
 	{
 		public static void log(this CodeInstruction ci) => $"{ci.opcode} {ci.operand}".log();
 
-		public static void log(this IEnumerable<CodeInstruction> cins, bool searchFirstOps = false)
+		public static void log(this IEnumerable<CodeInstruction> cins, string filename = null, bool printIndexes = true, bool printFirst = false)
 		{
+			var sb = new StringBuilder();
 			var list = cins.ToList();
 
 			int _findLabel(object label) => // find target index for jumps
 				list.FindIndex(_ci => _ci.labels?.FindIndex(l => l.Equals(label)) != -1);
+
+			static string _label2Str(Label label) => $"Label{label.GetHashCode()}";
 
 			static string _labelsInfo(CodeInstruction ci)
 			{
 				if ((ci.labels?.Count ?? 0) == 0)
 					return "";
 
-				string res = $"=> labels({ci.labels.Count}): ";
-				ci.labels.ForEach(l => res += "Label" + l.GetHashCode() + " ");
+				string res = $" => labels({ci.labels.Count}): ";
+				ci.labels.ForEach(l => res += _label2Str(l) + " ");
 
 				return res;
 			}
@@ -39,10 +42,21 @@ namespace Common.Harmony
 				var ci = list[i];
 
 				int labelIndex = (ci.operand is Label)? _findLabel(ci.operand): -1;
-				string operandInfo = labelIndex != -1? "jump to " + labelIndex: ci.operand?.ToString();
-				string isFirstOp = (searchFirstOps && list.FindIndex(_ci => _ci.opcode == ci.opcode) == i)? " 1ST":""; // is such an opcode is first encountered in this instruction
+				string operandInfo = labelIndex == -1? ci.operand?.ToString(): $"jump to {(printIndexes? labelIndex.ToString(): "")}({_label2Str(ci.operand.cast<Label>())})";
+				string isFirstOp = (printFirst && list.FindIndex(_ci => _ci.opcode == ci.opcode) == i)? " 1ST":""; // is such an opcode is first encountered in this instruction
+				string prefix = printIndexes? $"{i:D3}{isFirstOp}: ": "";
 
-				$"{i}{isFirstOp}: {ci.opcode} {operandInfo} {_labelsInfo(ci)}".log();
+				sb.AppendLine($"{prefix}{ci.opcode} {operandInfo}{_labelsInfo(ci)}");
+			}
+
+			if (filename == null)
+			{
+				sb.Insert(0, Environment.NewLine);
+				sb.ToString().log();
+			}
+			else
+			{
+				sb.ToString().saveToFile(filename);
 			}
 		}
 	}

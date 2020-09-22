9296a0020592e3d92857143270752e9c2a5f2161
zorgesho <53096970+zorgesho@users.noreply.github.com>
2020-09-23 00:04:48 +0300

    PrawnSuitSonarUpgrade 1.1.0

--- PrawnSuitSonarUpgrade/properties/AssemblyInfo.cs
+++ PrawnSuitSonarUpgrade/properties/AssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("PrawnSuitSonarUpgrade")]
 [assembly: AssemblyProduct("PrawnSuitSonarUpgrade")]
-[assembly: AssemblyCopyright("© 2019 zorgesho")]
+[assembly: AssemblyCopyright("© 2019-2020 zorgesho")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.1.0.0")]

498131467a9fbb54606981b2573e5569ba70aff1
zorgesho <53096970+zorgesho@users.noreply.github.com>
2020-12-09 11:08:14 +0300

    PrawnSuitJetUpgrade 1.1.0.BZ

--- PrawnSuitJetUpgrade/src/ThrusterOptimizerModule.cs
+++ PrawnSuitJetUpgrade/src/ThrusterOptimizerModule.cs
@@ -1,5 +1,4 @@
-﻿using SMLHelper.V2.Crafting;
-using Common.Crafting;
+﻿using Common.Crafting;
 
 namespace PrawnSuitJetUpgrade
 {
@@ -7,25 +6,28 @@ namespace PrawnSuitJetUpgrade
 	{
 		public static new TechType TechType { get; private set; } = 0;
 
-		protected override TechData getTechData() => new TechData
+		protected override TechInfo getTechInfo() => new TechInfo
 		(
-			new Ingredient(TechType.AdvancedWiringKit, 1),
-			new Ingredient(TechType.Sulphur, 3),
-			new Ingredient(TechType.Aerogel, 2),
-			new Ingredient(TechType.Polyaniline, 1)
-		)	{ craftAmount = 1 };
+			new TechInfo.Ing(TechType.AdvancedWiringKit),
+			new TechInfo.Ing(TechType.Sulphur, 3),
+			new TechInfo.Ing(TechType.Aerogel, 2),
+			new TechInfo.Ing(TechType.Polyaniline)
+		);
 
-		protected override void initPrefabPool() => addPrefabToPool(TechType.VehicleArmorPlating);
+		protected override void initPrefabPool() => addPrefabToPool(TechType.ExosuitJetUpgradeModule);
 
 		public override void patch()
 		{
 			TechType = register(L10n.ids_optimizerName, L10n.ids_optimizerDesc);
 
 			addToGroup(TechGroup.VehicleUpgrades, TechCategory.VehicleUpgrades);
-			addCraftingNodeTo(CraftTree.Type.SeamothUpgrades, "ExosuitModules");
+#if GAME_SN
+			addCraftingNodeTo(CraftTree.Type.SeamothUpgrades, "ExosuitModules", TechType.ExosuitJetUpgradeModule);
+#elif GAME_BZ
+			addCraftingNodeTo(CraftTree.Type.Fabricator, "Upgrades/ExosuitUpgrades", TechType.ExosuitJetUpgradeModule);
+#endif
 			setEquipmentType(EquipmentType.ExosuitModule, QuickSlotType.Passive);
-
-			setTechTypeForUnlock(TechType.BaseUpgradeConsole);
+			setTechTypeForUnlock(TechType.ExosuitJetUpgradeModule);
 		}
 	}
 }

ece2be7a45792447133873ebb8197b08892e4978
zorgesho <53096970+zorgesho@users.noreply.github.com>
2021-05-11 14:37:01 +0300

    console commands minor changes (ui_setoptionsspacing added)

--- UITweaks/src/ConsoleCommands.cs
+++ UITweaks/src/ConsoleCommands.cs
@@ -1,15 +1,18 @@
-﻿using System.Linq;
-using UnityEngine.UI;
-using Common;
+﻿using Common;
+using Common.Configuration;
+
+#if DEBUG
+using System.Linq;
+#endif
 
 namespace UITweaks
 {
 	class ConsoleCommands: PersistentConsoleCommands
 	{
-		public void ui_setpingenabled(PingType pingType, int colorIndex, bool enabled)
-		{
+		public void ui_setpingenabled(PingType pingType, int colorIndex, bool enabled) =>
 			PingToggles.setPingEnabled(pingType, colorIndex, enabled);
-		}
+
+		public void ui_setoptionsspacing(float spacing) => Options.Utils.setOptionsSpacing(spacing);
 
 #if DEBUG
 		public void ui_log_known_blueprints()
@@ -21,23 +24,11 @@ namespace UITweaks
 				foreach (var tech in entry.Value.entries)
 					$"{i++} {tech} {tech.Value._progress?.amount} {tech.Value._progress?.total} {tech.Value._progress?.unlocked}".logDbg();
 
-			int blueprintCount = blueprintsTab?.entries.
-									SelectMany(entry => entry.Value.entries).
-									Where(entry => entry.Value?._progress.total != -1).
-									Count() ?? 0;
-
+			int blueprintCount = blueprintsTab?.entries.SelectMany(entry => entry.Value.entries).Where(entry => entry.Value?._progress.total != -1).Count() ?? 0;
 			$"blueprintCount: {blueprintCount}".onScreen().logDbg();
 		}
 
-		public void ui_test_tooltip()
-		{
-			var tooltip = uGUI_Tooltip.main.gameObject;
-
-			var text = tooltip.createChild(tooltip.getChild("Text"), "TextBottom");
-			text.GetComponent<Text>().text = "Test text";
-
-			tooltip.dump("tooltip");
-		}
+		public void ui_dump_tooltip() => uGUI_Tooltip.main.gameObject?.dump("tooltip");
 
 		public void ui_tooltipclear() => uGUI_Tooltip.Clear();
 

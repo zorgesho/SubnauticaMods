b5c71cabbcefa01ad8f64ab71480bd83afab5c8c
zorgesho <53096970+zorgesho@users.noreply.github.com>
2021-06-25 07:25:49 +0300

    csproj cleanup

--- post-build.bat
+++ post-build.bat
@@ -13,8 +13,6 @@ echo %1 | findstr /c:"SN" 1>nul && set qmods_path="c:\games\subnautica\QMods"
 echo %1 | findstr /c:"BZ" 1>nul && set qmods_path="c:\games\subnautica.bz\QMods"
 if not exist %qmods_path% goto :eof
 
-echo %2.pdb > ..\.pdb-ignore
-
 :: adding info file to version for publish 
 echo %1 | findstr /c:"publish" 1>nul && copy nul %5%tmp_info_file% > nul
 
@@ -22,5 +20,4 @@ echo %1 | findstr /c:"publish" 1>nul && copy nul %5%tmp_info_file% > nul
 if exist mod.*.json move mod.*.json mod.json > nul
 
 echo =====
-xcopy %5*.* %qmods_path%\%2\ /e /y /exclude:..\.pdb-ignore
-del ..\.pdb-ignore
+xcopy %5*.* %qmods_path%\%2\ /e /y

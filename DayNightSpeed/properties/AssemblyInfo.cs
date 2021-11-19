4d1b4083cad7b00a0455f6db4e999fc6b5938a0b
zorgesho <53096970+zorgesho@users.noreply.github.com>
2021-11-19 14:22:29 +0300

    DayNightSpeed [SN/BZ] 1.3.1

--- DayNightSpeed/properties/AssemblyInfo.cs
+++ DayNightSpeed/properties/AssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("DayNightSpeed")]
 [assembly: AssemblyProduct("DayNightSpeed")]
-[assembly: AssemblyCopyright("© 2019-2020 zorgesho")]
+[assembly: AssemblyCopyright("© 2019-2021 zorgesho")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.1.0")]

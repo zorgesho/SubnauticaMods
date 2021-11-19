b235df70ae5a645a32ff2fc2b14506bca5f92402
zorgesho <53096970+zorgesho@users.noreply.github.com>
2021-11-19 15:02:37 +0300

    CustomHotkeys [SN/BZ] 1.1.0

--- CustomHotkeys/properties/AssemblyInfo.cs
+++ CustomHotkeys/properties/AssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("CustomHotkeys")]
 [assembly: AssemblyProduct("CustomHotkeys")]
-[assembly: AssemblyCopyright("© 2020 zorgesho")]
+[assembly: AssemblyCopyright("© 2020-2021 zorgesho")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]

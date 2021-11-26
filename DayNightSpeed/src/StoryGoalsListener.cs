c7cb00de5529d4fbd3cb37ef55a8043df9462998
zorgesho <53096970+zorgesho@users.noreply.github.com>
2021-11-26 19:12:48 +0300

    ignoring empty story goals or goals with zero delay

--- DayNightSpeed/src/StoryGoalsListener.cs
+++ DayNightSpeed/src/StoryGoalsListener.cs
@@ -9,6 +9,10 @@ using Common;
 using Common.Harmony;
 using Common.GameSerialization;
 
+#if DEBUG
+using System.Linq;
+#endif
+
 namespace DayNightSpeed
 {
 	static partial class DayNightSpeedControl
@@ -20,7 +24,9 @@ namespace DayNightSpeed
 			const string saveName = "goals";
 
 			static StoryGoalsListener instance = null;
+
 			List<string> goals = new(); // goals with delay less than shortGoalDelay
+			void updateForcedMode() => forcedNormalSpeed = (goals.Count != 0);
 
 			SaveLoadHelper slHelper;
 			class SaveData { public List<string> goals; }
@@ -40,14 +46,37 @@ namespace DayNightSpeed
 
 			void OnDestroy() => instance = null;
 
-			void Update() => slHelper.update();
+			void Update()
+			{
+				slHelper.update();
+#if GAME_BZ
+				cleanUpGoals();
+#endif
+#if DEBUG
+				if (goals.Count > 0 && Main.config.dbgCfg.enabled && Main.config.dbgCfg.showGoals)
+					string.Join(" ", goals.Select(goal => $"'{goal}'")).onScreen("goals listener");
+#endif
+			}
 
+#if GAME_BZ // in BZ, goals often ignoring 'timeExecute', so we need to remove them here
+			void cleanUpGoals()
+			{
+				if (!StoryGoalScheduler.main || !DayNightCycle.main)
+					return;
+
+				static bool _isGoalExpired(string goal) =>
+					StoryGoalScheduler.main.GetScheduledGoal(goal)?.timeExecute < DayNightCycle.main.timePassed + float.Epsilon;
+
+				goals.RemoveAll(goal => goal == "" || _isGoalExpired(goal));
+				updateForcedMode();
+			}
+#endif
 			public static void load() => instance?.onLoad();
 
 			void onLoad()
 			{
 				goals = SaveLoad.load<SaveData>(saveName)?.goals ?? new List<string>();
-				forcedNormalSpeed = (goals.Count != 0);
+				updateForcedMode();
 			}
 
 			void onSave()
@@ -57,22 +86,24 @@ namespace DayNightSpeed
 
 			public static bool isGoalCompleted(string goalKey) => StoryGoalManager.main.completedGoals.Contains(goalKey);
 
+			static bool shouldIgnoreGoal(StoryGoal goal) =>
+				goal == null || goal.key.isNullOrEmpty() || goal.delay == 0f || goal.delay > shortGoalDelay;
+
 			[HarmonyPostfix, HarmonyPatch(typeof(StoryGoalScheduler), "Schedule")]
 			static void onAddGoal(StoryGoal goal)
 			{
-				if (goal == null || goal.delay > shortGoalDelay || isGoalCompleted(goal.key))
+				if (shouldIgnoreGoal(goal) || isGoalCompleted(goal.key))
 					return;
 
 				forcedNormalSpeed = true;
-
 				instance.goals.Add(goal.key);											$"StoryGoalsListener: goal added '{goal.key}'".logDbg();
 			}
 
 			[HarmonyPostfix, HarmonyPatch(typeof(StoryGoal), "Execute")]
 			static void onRemoveGoal(string key)
 			{
-				if (instance.goals.RemoveAll(g => g == key) > 0 && instance.goals.Count == 0)
-					forcedNormalSpeed = false;
+				instance.goals.RemoveAll(g => g == key);
+				instance.updateForcedMode();
 			}
 		}
 	}

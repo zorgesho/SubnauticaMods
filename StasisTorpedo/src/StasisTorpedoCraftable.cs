32e29b7081925d451391e26e065f7cb55677b2ea
zorgesho <53096970+zorgesho@users.noreply.github.com>
2021-11-29 20:17:25 +0300

    craftable object for stasis torpedo

---
+++ StasisTorpedo/src/StasisTorpedoCraftable.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using SMLHelper.V2.Assets;
+
+using Common.Crafting;
+
+namespace StasisTorpedo
+{
+	class StasisTorpedo: PoolCraftableObject
+	{
+		static GameObject stasisSpherePrefab;
+
+		public static new TechType TechType { get; private set; } = 0;
+
+		protected override TechInfo getTechInfo() => new // TODO
+		(
+			new (TechType.Titanium),
+			new (TechType.Gold)
+		);
+
+		protected override void initPrefabPool()
+		{
+			addPrefabToPool(TechType.GasTorpedo);
+			addPrefabToPool(TechType.StasisRifle);
+		}
+
+		protected override GameObject getGameObject(GameObject[] prefabs)
+		{
+			stasisSpherePrefab ??= ModPrefabCache.AddPrefabCopy(prefabs[1].GetComponent<StasisRifle>().effectSpherePrefab, false);
+			return prefabs[0];
+		}
+
+		public override void patch()
+		{
+			TechType = register(); // TODO
+
+			addToGroup(TechGroup.VehicleUpgrades, TechCategory.VehicleUpgrades); // TODO check for BZ
+			addCraftingNodeTo(CraftTree.Type.SeamothUpgrades, "Torpedoes");
+			unlockOnStart(); // TODO
+		}
+	}
+}

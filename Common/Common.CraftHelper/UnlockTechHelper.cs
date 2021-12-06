84c1d6c7980711e65b20f3252d94f9fe09cb716d
zorgesho <53096970+zorgesho@users.noreply.github.com>
2021-12-06 14:30:33 +0300

    minor refactoring for UnlockTechHelper

--- Common/Common.CraftHelper/UnlockTechHelper.cs
+++ Common/Common.CraftHelper/UnlockTechHelper.cs
@@ -18,11 +18,9 @@ namespace Common.Crafting
 		// key - tech for unlocking, value - tech for unlockPopup sprite (can be tech type or fragment type)
 		static readonly Dictionary<TechType, TechType> unlockPopups = new();
 
-		static readonly HarmonyHelper.LazyPatcher patcher = new();
-
 		public static void setFragmentTypeToUnlock(TechType unlockTechType, TechType origFragTechType, TechType substFragTechType, int fragCount, float scanTime)
 		{
-			patcher.patch();
+			FragmentUnlockPatches.patcher.patch();
 
 			PDAHandler.AddCustomScannerEntry(new PDAScanner.EntryData()
 			{
@@ -36,68 +34,61 @@ namespace Common.Crafting
 			});
 
 			fragments[origFragTechType] = substFragTechType;
-			setUnlockNotification(unlockTechType, origFragTechType);
-		}
+			unlockPopups[unlockTechType] = origFragTechType;
 
-		public static void setUnlockNotification(TechType techType, UnityEngine.Sprite unlockPopup)
-		{
-			KnownTechHandler.SetAnalysisTechEntry(techType, new TechType[0], UnlockSprite: unlockPopup);
+			KnownTechHandler.SetAnalysisTechEntry(unlockTechType, new TechType[0]);
 		}
-
-		public static void setUnlockNotification(TechType techType, TechType unlockPopupTechType)
-		{
-			setUnlockNotification(techType, null);
-			unlockPopups[techType] = unlockPopupTechType;
-		}
-
 
 		#region patches
-
-		[HarmonyTranspiler, HarmonyPatch(typeof(PDAScanner), "Scan")]
-		static IEnumerable<CodeInstruction> scannerPatch(IEnumerable<CodeInstruction> cins)
+		static class FragmentUnlockPatches
 		{
-			static TechType _substTechType(TechType scanTechType) // substitute fragment tech type if it already known
+			public static readonly HarmonyHelper.LazyPatcher patcher = new();
+
+			[HarmonyTranspiler, HarmonyPatch(typeof(PDAScanner), "Scan")]
+			static IEnumerable<CodeInstruction> scannerPatch(IEnumerable<CodeInstruction> cins)
 			{
-				if (!fragments.TryGetValue(scanTechType, out TechType substTechType))
-					return scanTechType;
+				static TechType _substTechType(TechType scanTechType) // substitute fragment tech type if it already known
+				{
+					if (!fragments.TryGetValue(scanTechType, out TechType substTechType))
+						return scanTechType;
 
-				return PDAScanner.complete.Contains(scanTechType)? substTechType: scanTechType;
-			}
+					return PDAScanner.complete.Contains(scanTechType)? substTechType: scanTechType;
+				}
 
-			return CIHelper.ciInsert(cins,
-				cin => cin.isOp(OpCodes.Stloc_0), +1, 1,
+				return CIHelper.ciInsert(cins, ci => ci.isOp(OpCodes.Stloc_0), +1, 1,
 					OpCodes.Ldloc_0,
 					CIHelper.emitCall<Func<TechType, TechType>>(_substTechType),
 					OpCodes.Stloc_0);
-		}
+			}
 
-		[HarmonyPrefix, HarmonyPatch(typeof(PDAScanner), "ContainsCompleteEntry")] // for loot spawning
-		static bool fragmentCheckOverride(TechType techType, ref bool __result)
-		{
-			if (!fragments.TryGetValue(techType, out TechType substTechType))
-				return true;
-
-			__result = PDAScanner.complete.Contains(substTechType);
-			return false;
-		}
-
-		[HarmonyPriority(Priority.Low)]
-		[HarmonyPostfix, HarmonyHelper.Patch(typeof(KnownTech), "Initialize")]
-		static void unlockPopupsUpdate()
-		{
-			KnownTech.AnalysisTech _getEntry(TechType techType) =>
-				KnownTech.analysisTech.FirstOrDefault(tech => tech.techType == techType);
-
-			foreach (var popup in unlockPopups)
+			[HarmonyPrefix, HarmonyPatch(typeof(PDAScanner), "ContainsCompleteEntry")] // for loot spawning
+			static bool fragmentCheckOverride(TechType techType, ref bool __result)
 			{
-				var tech   = _getEntry(popup.Key);
-				var sprite = _getEntry(popup.Value)?.unlockPopup;
+				if (!fragments.TryGetValue(techType, out TechType substTechType))
+					return true;
 
-				if (sprite == null && PDAScanner.GetEntryData(popup.Value) is PDAScanner.EntryData fragData)
-					sprite = _getEntry(fragData.blueprint)?.unlockPopup; // try popup.Value as fragment tech type
+				__result = PDAScanner.complete.Contains(substTechType);
+				return false;
+			}
 
-				if (sprite != null)
-					tech.unlockPopup = sprite;
+			[HarmonyPriority(Priority.Low)]
+			[HarmonyPostfix, HarmonyHelper.Patch(typeof(KnownTech), "Initialize")]
+			static void unlockPopupsUpdate()
+			{
+				KnownTech.AnalysisTech _getEntry(TechType techType) =>
+					KnownTech.analysisTech.FirstOrDefault(tech => tech.techType == techType);
+
+				foreach (var popup in unlockPopups)
+				{
+					var tech = _getEntry(popup.Key);
+					var sprite = _getEntry(popup.Value)?.unlockPopup;
+
+					if (sprite == null && PDAScanner.GetEntryData(popup.Value) is PDAScanner.EntryData fragData)
+						sprite = _getEntry(fragData.blueprint)?.unlockPopup; // try popup.Value as fragment tech type
+
+					if (sprite != null)
+						tech.unlockPopup = sprite;
+				}
 			}
 		}
 		#endregion

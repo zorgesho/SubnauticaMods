d3a2bb19151546e98ef0b96e950423b158e06e18
zorgesho <53096970+zorgesho@users.noreply.github.com>
2022-04-01 01:57:01 +0300

    GravTrapImproved [SN/BZ] 2.2.0

--- GravTrapImproved/properties/AssemblyInfo.cs
+++ GravTrapImproved/properties/AssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("GravTrapImproved")]
 [assembly: AssemblyProduct("GravTrapImproved")]
-[assembly: AssemblyCopyright("© 2019-2021 zorgesho")]
+[assembly: AssemblyCopyright("© 2019-2022 zorgesho")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]

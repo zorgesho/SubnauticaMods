bdd4e7f883af65b623bc8e13738d498fab1d2b14
zorgesho <53096970+zorgesho@users.noreply.github.com>
2022-05-02 18:28:13 +0300

    compatibility patch

---
+++ UITweaks/src/storage-tweaks/patches/CompatibilityPatches.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection.Emit;
+using System.Collections.Generic;
+
+using HarmonyLib;
+
+using Common.Harmony;
+
+namespace UITweaks.StorageTweaks
+{
+	[OptionalPatch, PatchClass]
+	static class CompatibilityPatches
+	{
+		static bool prepare()
+		{
+			var st = Main.config.storageTweaks;
+			return st.enabled && (st.allInOneActions || st.showContentsInfo);
+		}
+
+		[HarmonyTranspiler, HarmonyHelper.Patch(HarmonyHelper.PatchOptions.CanBeAbsent)]
+		[HarmonyHelper.Patch("Tweaks_Fixes.Storage_Patch+StorageContainer_Patch, Tweaks and Fixes", "OnHandHoverPrefix")]
+		static IEnumerable<CodeInstruction> patchDisabler(IEnumerable<CodeInstruction> _) =>
+			CIHelper.toCIList(OpCodes.Ldc_I4_1, OpCodes.Ret);
+	}
+}

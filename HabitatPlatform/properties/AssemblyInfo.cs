3c6681d4145d2b3756c49a4b2deeff5e1f4c04f7
zorgesho <53096970+zorgesho@users.noreply.github.com>
2022-05-03 16:52:00 +0300

    HabitatPlatform [SN] 1.0.3

--- HabitatPlatform/properties/AssemblyInfo.cs
+++ HabitatPlatform/properties/AssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("HabitatPlatform")]
 [assembly: AssemblyProduct("HabitatPlatform")]
-[assembly: AssemblyCopyright("© 2020-2021 zorgesho")]
+[assembly: AssemblyCopyright("© 2020-2022 zorgesho")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]

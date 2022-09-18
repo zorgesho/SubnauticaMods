a0158e4a540ab637095585568a6d936b7101a990
zorgesho <53096970+zorgesho@users.noreply.github.com>
2022-09-18 13:43:38 +0300

    WarningsDisabler [BZ] 1.2.3

--- WarningsDisabler/properties/AssemblyInfo.cs
+++ WarningsDisabler/properties/AssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("WarningsDisabler")]
 [assembly: AssemblyProduct("WarningsDisabler")]
-[assembly: AssemblyCopyright("© 2019-2021 zorgesho")]
+[assembly: AssemblyCopyright("© 2019-2022 zorgesho")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.2.2.0")]
+[assembly: AssemblyVersion("1.2.3.0")]
